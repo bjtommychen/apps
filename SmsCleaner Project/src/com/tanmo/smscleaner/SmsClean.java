0f6f9ce8459ba00c1e6d3380cb86ef85b2593fb0
ctao <ctao@736fafbb-d7fb-7b48-b8c4-fe59a0342e39>
2011-06-01 10:02:48 +0000

    using SimpleAdapter, work well.

--- SmsCleaner Project/src/com/tanmo/smscleaner/SmsClean.java
+++ SmsCleaner Project/src/com/tanmo/smscleaner/SmsClean.java
@@ -1,5 +1,6 @@
 package com.tanmo.smscleaner;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -7,16 +8,15 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.net.Uri;
 import android.database.Cursor;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import android.app.ListActivity;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.graphics.Color;
+import android.graphics.Typeface;
 
-public class SmsClean extends ListActivity
+import java.util.*;
+import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+
+public class SmsClean extends Activity
 {
 	private String TAG = "smsclean";
 	private String info, dbginfo, info_show;
@@ -24,6 +24,9 @@ public class SmsClean extends ListActivity
 	private Uri uri;
 	private List<String> arraylist_sms = new ArrayList<String>();
 	private ArrayAdapter<String> arrayadapter_sms;
+	private ListView list;
+	private TextView footview;
+	ArrayList<HashMap<String, Object>> sms_array1 = new ArrayList<HashMap<String, Object>>();
 
 	// private ListView list;
 
@@ -34,9 +37,13 @@ public class SmsClean extends ListActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		// list=(ListView)findViewById(R.id.listView1);
+		list = (ListView) findViewById(R.id.listView1);
+		list.setBackgroundColor(Color.BLUE);
+		list.setCacheColorHint(Color.BLUE);// Tommy: won't change color when
+		// select
 		Log.i(TAG, "start !");
 
+		// Show list
 		browse_sms(20);
 
 		Log.i(TAG, "stop !");
@@ -50,11 +57,20 @@ public class SmsClean extends ListActivity
 		if (num == 0)
 			num = 99999;
 
-//		String[] sw = new String[100];
-//		for (i = 0; i < 100; i++)
-//		{
-//			sw[i] = "listtest_" + i;
-//		}
+		// String[] sw = new String[100];
+		// for (i = 0; i < 100; i++)
+		// {
+		// sw[i] = "listtest_" + i;
+		// }
+
+		// for (i = 0; i < 10; i++)
+		// {
+		// HashMap<String, Object> map = new HashMap<String, Object>();
+		// map.put("ADDR", "Test Title");
+		// map.put("BODY", "listtest_" + i);
+		// sms_array1.add(map);
+		// }
+
 		i = 1;
 		arraylist_sms.clear();
 
@@ -67,25 +83,35 @@ public class SmsClean extends ListActivity
 		} else
 		{
 			Log.i(TAG, "Total " + cur.getCount() + " sms !!!");
-			arraylist_sms.add("Total " + cur.getCount() + " sms !!!");
+			// arraylist_sms.add("Total " + cur.getCount() + " sms !!!");
+
+			footview = new TextView(this);
+			footview.setText("Total " + cur.getCount() + " sms !!!");
+			footview.setTextColor(Color.RED);
+			footview.setTypeface(null, Typeface.BOLD);
+			list.addFooterView(footview);
+			list.addHeaderView(footview);
 
 			if (cur.moveToFirst())
 			{
 				do
 				{
+					HashMap<String, Object> map = new HashMap<String, Object>();
+					
 					info = "Sms " + String.valueOf(i) + ": ";
 					info_show = "";
 					for (int j = 0; j < cur.getColumnCount(); j++)
 					{
-
 						if (cur.getColumnName(j).equals("body"))
 						{
 							Log.i(TAG, "Msg is " + cur.getString(j));
 							info_show += cur.getString(j);
+							map.put("BODY", cur.getString(j));
 						} else if (cur.getColumnName(j).equals("address"))
 						{
 							Log.i(TAG, "From " + cur.getString(j));
 							info_show += "From:" + cur.getString(j) + "\n";
+							map.put("ADDR","From:" + cur.getString(j));
 						} else
 						{
 							info += cur.getColumnName(j) + "="
@@ -94,24 +120,38 @@ public class SmsClean extends ListActivity
 					}
 					Log.i(TAG, info);
 					arraylist_sms.add(info_show);
+					sms_array1.add(map);
 				} while (cur.moveToNext() && (i++ < num));
 			}
 		}
 
-		// if extends ListActivity, Tommy: this mode, can't change textsize.
-		if (true)
-		{
-			arrayadapter_sms = new ArrayAdapter<String>(this,
-					android.R.layout.simple_list_item_multiple_choice,
-					arraylist_sms);
-			setListAdapter(arrayadapter_sms);
-			final ListView listView = getListView();
-			listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+		{ // if extends ListActivity, Tommy: this mode, can't change textsize.
+			// arrayadapter_sms = new ArrayAdapter<String>(this,
+			// android.R.layout.simple_list_item_multiple_choice,
+			// arraylist_sms);
+			// setListAdapter(arrayadapter_sms);
+			// final ListView listView = getListView();
+			// listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 		}
-		
-		
+
+		{ // Use customer listview
 		// arrayadapter_sms = new ArrayAdapter<String>(this,
-		// R.layout.smslist_layout, sw);
+		// R.layout.smslist_layout, arraylist_sms);
+		// list.setAdapter(arrayadapter_sms);
+		}
+
+		// Use SimpleAdapter
+		{
+			SimpleAdapter smslistadapter = new SimpleAdapter(this, sms_array1,
+					R.layout.smslist_map, new String[] { "ADDR", "BODY" },
+					new int[] { R.id.sms_address, R.id.sms_body });
+
+			list.setAdapter(smslistadapter);
+		}
+
+		Toast.makeText(getApplicationContext(),
+				"Total " + cur.getCount() + " sms !!!", Toast.LENGTH_LONG)
+				.show();
 
 		// listView.setItemsCanFocus(false);
 
