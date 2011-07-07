fdb4fadaa3a41501981ea05e0bf02e64bca7aa4a
ctao <ctao@736fafbb-d7fb-7b48-b8c4-fe59a0342e39>
2011-07-07 08:23:05 +0000

    enable NEON.

--- AudioTest/jni/Android.mk
+++ AudioTest/jni/Android.mk
@@ -17,9 +17,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 #thumb arm
-#LOCAL_ARM_MODE := arm
+LOCAL_ARM_MODE := arm
 #armeabi armeabi-v7a x86
-#TARGET_ARCH_ABI := armeabi armeabi-v7a
+TARGET_ARCH_ABI := armeabi-v7a
 #TARGET_PLATFORM = android-7
 
 LOCAL_MODULE    := mp3dec
@@ -28,8 +28,7 @@ LOCAL_CFLAGS := -DHAVE_CONFIG_H -DFPM_DEFAULT -DDECODE_GRANULE -DHUFF_OPT -DRMS_
 				-DSYNTH_USE_TABLE_EVEN_ODD_NEW -D_MSC_VER 
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-	LOCAL_CFLAGS += -DOPT_TOMMY_NEON -mfloat-abi=softfp -mfpu=neon 
-#    LOCAL_CFLAGS := -DHAVE_NEON=1
+	LOCAL_CFLAGS += -DOPT_TOMMY_NEON -mfloat-abi=softfp -mfpu=neon -DHAVE_NEON=1
 #    LOCAL_SRC_FILES += helloneon-intrinsics.c.neon
 endif
 
