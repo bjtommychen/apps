eaa2c63cdb88aa3d685cc32b703fae7539180acf
tommy <tommy@tu>
2012-02-06 20:55:10 +0800

    add

--- YuvShow/src/YuvShow_SDL.c
+++ YuvShow/src/YuvShow_SDL.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello yuvshow World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
