f23b2d4f75b6834cac72b60af387e082469386ef
tommy <tommy@tu>
2012-02-06 21:39:11 +0800

    add 3

--- YuvShow/src/YuvShow_SDL.c
+++ YuvShow/src/YuvShow_SDL.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello yuvshow  2 World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello yuvshow  23 World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
