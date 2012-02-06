3031f9eb28eab9728940267336f66fc15e3b0c4d
tommy <tommy@tu>
2012-02-06 21:34:28 +0800

    add 2

--- YuvShow/src/YuvShow_SDL.c
+++ YuvShow/src/YuvShow_SDL.c
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello yuvshow World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello yuvshow  2 World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
