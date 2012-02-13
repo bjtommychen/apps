6d9732c2415aeafc90191b04983592f1a8941169
tommy <tommy@test.com>
2012-02-13 18:12:42 +0800

    add

--- ffplay_lite/src/av_io.c
+++ ffplay_lite/src/av_io.c
@@ -16,7 +16,7 @@
 #define DEBUG
 
 #ifdef DEBUG
-#define log(a, b...)	fprintf(stdout, a, ##b)
+#define log(a, b...)	fprintf(stdout, "AVIO: "a, ##b)
 #else
 #define log(a, b...)
 #endif
@@ -28,30 +28,67 @@
 /******************************************************************************/
 /*  Local Variables                                                           */
 /******************************************************************************/
-int bytes_per_component;
-int chNum = 2, SampleRate = 44100, Bits_per_Sample = 16;
+SDL_AudioSpec *desired, *obtained;
 SDL_Surface *screen, *bitmap;
+
+int chNum = 2, SampleRate = 44100, Bits_per_Sample = 16;
 int average;
-int outputAudioBufferSize = 0;
-unsigned char *buff;
+int outputAudioabufferSize = 0;
+unsigned char *abuff;
+
 /******************************************************************************/
 /*  Local Function Declarations                                               */
 /******************************************************************************/
-static void mixaudio(void *usr_used, Uint8 * stream, int len)
+
+//void (SDLCALL *callback)(void *userdata, Uint8 *stream, int len);
+static void sdl_audio_callback(void *userdata, Uint8 *stream, int len)
 {
 	int i;
-//	int ch0, ch1;
-//	Uint16 outputValue;
-	Uint16 *p16out = (Uint16 *) stream;
-	Uint16 *pSrc16 = (Uint16 *) buff;
+	int (*filldata)(Uint8 *stream, int len);
 
+	log("enter sdl_audio_callback().\n");
+	filldata = userdata;
+	(*filldata)(stream, len);
+	log("leave sdl_audio_callback().\n");
 }
 
+/******************************************************************************/
+/*  Function Definitions                                                      */
+/******************************************************************************/
 
-static int avio_init_audio()
+int AVIO_Init()
+{
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) < 0)
+	{
+		log("Unable to init SDL: %s\n", SDL_GetError());
+		return 1;
+	}
+	log("AVIO init.\n");
+}
+
+int AVIO_Exit()
+{
+	//Audio
+	free(abuff);
+	SDL_CloseAudio();
+
+	//Video
+	SDL_Quit();
+}
+
+int AVIO_InitAudio(int ch, int srate, int bps, void *callback)
 {
 	/* setup audio */
-	SDL_AudioSpec *desired, *obtained;
+
+	if (ch)
+		chNum = ch;
+	if (srate)
+		SampleRate = srate;
+	if (bps)
+		Bits_per_Sample = bps;
+
+	log("Init SDL Audio, ch:%d, srate:%d, bps:%d\n",
+			chNum, SampleRate, Bits_per_Sample);
 
 	/* Allocate a desired SDL_AudioSpec */
 	desired = (SDL_AudioSpec *) malloc(sizeof(SDL_AudioSpec));
@@ -70,71 +107,69 @@ static int avio_init_audio()
 		desired->format = AUDIO_S8;
 	}
 
-	/* Large audio buffers reduces risk of dropouts but increases response time.
+	/* Large audio abuffers reduces risk of dropouts but increases response time.
 
-	 * You should always check if you actually GOT the audiobuffer size you wanted,
-	 * note that not hardware supports all buffer sizes (< 2048 bytes gives problems with some
+	 * You should always check if you actually GOT the audioabuffer size you wanted,
+	 * note that not hardware supports all abuffer sizes (< 2048 bytes gives problems with some
 	 * hardware). Older versions of SDL had a bug that caused many configuration to use a
-	 * buffersize of 11025 bytes, if your sdl.dll is approx. 1 Mb in stead of 220 Kb, download
+	 * abuffersize of 11025 bytes, if your sdl.dll is approx. 1 Mb in stead of 220 Kb, download
 	 * v1.2.8 of SDL or better...)
 	 */
 	desired->samples = 2048;
 
 	/* Our callback function */
-	desired->callback = mixaudio;
-	desired->userdata = NULL;
+	desired->callback = sdl_audio_callback;
+	desired->userdata = callback;
 
 	desired->channels = chNum;
 
 	/* Open the audio device and start playing sound! */
 	if (SDL_OpenAudio(desired, obtained) < 0)
 	{
-		printf("AudioMixer, Unable to open audio: %s\n", SDL_GetError());
-		SDL_Quit();
-		return(1);
+		log("AudioMixer, Unable to open audio: %s\n", SDL_GetError());
+//		SDL_Quit();
+		return (1);
 	}
 
-	printf(" Obtained samples:%d, freq:%dHz, channel:%d \n", obtained->samples,
-			obtained->freq, obtained->channels);
-	printf(
-			" Obtained->format: %s %d bits\n",
+	log("Obtained samples:%d, freq:%dHz, channel:%d \n",
+			obtained->samples, obtained->freq, obtained->channels);
+	log(
+			"Obtained->format: %s %d bits\n",
 			(obtained->format & 0x8000) ? "signed":"unsigned", (obtained->format & 0x00ff));
 
 	/* if the format is 16 bit, two bytes are written for every sample */
 	if (obtained->format == AUDIO_U8 || obtained->format == AUDIO_S8)
 	{
-		outputAudioBufferSize = obtained->samples;
+		outputAudioabufferSize = obtained->samples;
 	}
 	else
 	{
-		outputAudioBufferSize = obtained->samples * 2;
+		outputAudioabufferSize = obtained->samples * 2;
 	}
 
 	if (obtained->channels == 2)
-		outputAudioBufferSize *= 2;
+		outputAudioabufferSize *= 2;
 
-	buff = (unsigned char *) malloc(outputAudioBufferSize + 10);
-	if (buff == 0)
-		exit(1);
-}
-
-/******************************************************************************/
-/*  Function Definitions                                                      */
-/******************************************************************************/
-
-int AVIO_Init()
-{
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) < 0)
+	abuff = (unsigned char *) malloc(outputAudioabufferSize + 10);
+	if (abuff == 0)
 	{
-		log("Unable to init SDL: %s\n", SDL_GetError());
-		return 1;
+		log("Can't alloc mem for abuff.\n");
+		return (1);
 	}
 
+//	SDL_Pause?Audio(0);
+//	while (1)
+	{
+//		SDL_Delay(1);
+	}
+
+	return 0;
 }
 
-int AVIO_Exit()
+int AVIO_PauseAudio(int pause)
 {
-	SDL_CloseAudio();
-	SDL_Quit();
+	log("PauseAudio %d\n", pause);
+	SDL_PauseAudio(pause);
+//	SDL_Delay(1);
+	return 0;
 }
-
