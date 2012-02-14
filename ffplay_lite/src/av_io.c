ab3320857aed37df117af3dbd9d04e85be9a9d01
tommy <tommy@test.com>
2012-02-14 18:19:03 +0800

    add yuv420 show.

--- ffplay_lite/src/av_io.c
+++ ffplay_lite/src/av_io.c
@@ -21,6 +21,11 @@
 #define log(a, b...)
 #endif
 
+enum YUVFORMAT
+{
+	YUV_420 = 0, YUV_422,
+};
+
 /******************************************************************************/
 /*  Local Type Definitions                                                    */
 /******************************************************************************/
@@ -30,11 +35,15 @@
 /******************************************************************************/
 SDL_AudioSpec *desired, *obtained;
 SDL_Surface *screen, *bitmap;
+SDL_Overlay *overlay = 0;
 
+// Audio
 int chNum = 2, SampleRate = 44100, Bits_per_Sample = 16;
 int average;
 int outputAudioabufferSize = 0;
 unsigned char *abuff = NULL;
+
+// Video
 
 /******************************************************************************/
 /*  Local Function Declarations                                               */
@@ -43,13 +52,48 @@ unsigned char *abuff = NULL;
 //void (SDLCALL *callback)(void *userdata, Uint8 *stream, int len);
 static void sdl_audio_callback(void *userdata, Uint8 *stream, int len)
 {
-	int i;
 	int (*filldata)(Uint8 *stream, int len);
 
 	log("enter sdl_audio_callback().\n");
 	filldata = userdata;
 	(*filldata)(stream, len);
 	log("leave sdl_audio_callback().\n");
+}
+
+static int avio_get_frame_yuv420(char *data[], int linesize[], int xsize,
+		int ysize)
+{
+	int x, y;
+	unsigned char *src, *dst;
+
+	// Y
+	src = data[0];
+	for (y = 0; y < ysize; y++)
+	{
+		dst = overlay->pixels[0] + overlay->pitches[0] * y;
+		memcpy(dst, src, linesize[0]);
+		src += linesize[0];
+	}
+
+	// U
+	src = data[1];
+	for (y = 0; y < ysize / 2; y++)
+	{
+		dst = overlay->pixels[1] + overlay->pitches[0] * y;
+		memcpy(dst, src, linesize[1]);
+		src += linesize[1];
+	}
+
+	// V
+	src = data[2];
+	for (y = 0; y < ysize; y++)
+	{
+		dst = overlay->pixels[2] + overlay->pitches[0] * y;
+		memcpy(dst, src, linesize[2]);
+		src += linesize[2];
+	}
+
+	return 0;
 }
 
 /******************************************************************************/
@@ -62,8 +106,8 @@ int AVIO_Init()
 	{
 		log("Unable to init SDL: %s\n", SDL_GetError());
 		return 1;
-	}
-	log("AVIO init.\n");
+	}log("AVIO init.\n");
+	return 0;
 }
 
 int AVIO_Exit()
@@ -74,7 +118,11 @@ int AVIO_Exit()
 	SDL_CloseAudio();
 
 	//Video
+	if (overlay)
+		SDL_FreeYUVOverlay(overlay);
+
 	SDL_Quit();
+	return 0;
 }
 
 int AVIO_InitAudio(int ch, int srate, int bps, void *callback)
@@ -174,3 +222,90 @@ int AVIO_PauseAudio(int pause)
 //	SDL_Delay(1);
 	return 0;
 }
+
+/*
+ * w, h is the overlay size
+ */
+int AVIO_InitYUV420(int w, int h, char *title)
+{
+	int i;
+	char titlebar[255] = "";
+	Uint32 video_flags, desired_bpp, overlay_format;
+
+	video_flags = SDL_HWSURFACE | SDL_ASYNCBLIT | SDL_HWACCEL;
+	if (0)
+		video_flags |= SDL_FULLSCREEN;
+	else
+		video_flags |= SDL_RESIZABLE;
+	desired_bpp = 0;
+
+	/* Initialize the display */
+	screen = SDL_SetVideoMode(w, h, desired_bpp, video_flags);
+	if (screen == NULL)
+	{
+		fprintf(stderr, " Couldn 't set %dx%dx%d video mode: %s\n", w, h,
+				desired_bpp, SDL_GetError());
+		exit(1);
+	}log(
+			"Set%s %dx%dx%d mode\n",
+			screen->flags & SDL_FULLSCREEN ? " fullscreen" : "", screen->w, screen->h, screen->format->BitsPerPixel);
+	log("(video surface located in %s memory)\n",
+			(screen->flags & SDL_HWSURFACE) ? "video" : "system");
+	if (screen->flags & SDL_DOUBLEBUF)
+	{
+		log("Double-buffering enabled\n");
+		//flip = 1;
+	}
+
+	/* Set the window manager title bar */
+	strcpy(titlebar, "file: ");
+	strcat(titlebar, title);
+	SDL_WM_SetCaption(titlebar, "by tommy");
+
+	/* Create the overlay */
+	overlay_format = SDL_IYUV_OVERLAY; // YUYV
+	overlay = SDL_CreateYUVOverlay(w, h, overlay_format, screen);
+
+	log(
+			"Created %dx%dx%d %s %s overlay\n",
+			overlay->w, overlay->h, overlay->planes, overlay->hw_overlay ? "hardware" : "software", overlay->format == SDL_YV12_OVERLAY ? "YV12" : overlay->format == SDL_IYUV_OVERLAY ? "IYUV" : overlay->format == SDL_YUY2_OVERLAY ? "YUY2" : overlay->format == SDL_UYVY_OVERLAY ? "UYVY" : overlay->format == SDL_YVYU_OVERLAY ? "YVYU" : "Unknown");
+	for (i = 0; i < overlay->planes; i++)
+	{
+		log("  plane %d: pitch=%d\n", i, overlay->pitches[i]);
+	}
+	return 0;
+}
+
+/*
+ * xsize, ysize is the decoded yuv display size.
+ */
+
+int AVIO_showYUV420(char *data[], int linesize[], int xsize, int ysize,
+		int format)
+{
+	SDL_LockYUVOverlay(overlay);
+
+	switch (format)
+	{
+//	case YUV_420:
+	default:
+		avio_get_frame_yuv420(data, linesize, xsize, ysize);
+		break;
+
+	}
+
+	SDL_UnlockYUVOverlay(overlay);
+
+	/* show */
+	{
+		SDL_Rect rect;
+
+		rect.w = overlay->w;
+		rect.h = overlay->h;
+		rect.x = 0;
+		rect.y = 0;
+		SDL_DisplayYUVOverlay(overlay, &rect);
+	}
+
+}
+
