18af90aadfee1ff66aab6945aa2d350a3159253d
tommy <chen.tao.tommy@gmail.com>
2012-02-24 11:59:23 +0800

    play avfile OK. after use my build ffmpeg armv7 version. ffmpeg lib from rockplayer has no avformat, dont' know why.

--- ffplay_android/test/example/example.c
+++ ffplay_android/test/example/example.c
@@ -42,7 +42,7 @@
 /******************************************************************************/
 /*  Local Function Declarations                                               */
 /******************************************************************************/
-static void show_banner(int argc, char **argv)
+static void show_libinfo(int argc, char **argv)
 {
 	printf("%s version %s by %s\t", APPNAME, VERSION, AUTHOR);
 	printf("%sbuilt on %s %s \n", " ", __DATE__, __TIME__);
@@ -57,14 +57,23 @@ static void show_banner(int argc, char **argv)
 /* Called from the main */
 int main(int argc, char **argv)
 {
-	show_banner(argc, argv);
+//	show_libinfo(argc, argv);
+
+//	show_banner(argc, argv, 0);
 
 	FFNDK_init();
 //	FFNDK_showlibinfo();
 
-	FFNDK_decode_video("/sdcard/srv/stream/love_mv.mpg.videostream", 0);
+//	FFNDK_decode_video("/sdcard/srv/stream/love_mv.mpg.videostream", 0);
 //	FFNDK_decode_video("/sdcard/srv/stream/BBC10m.videostream", 0);
 //	FFNDK_decode_video("/sdcard/srv/stream/vs.mp4.videostream", 0);
+
+//	FFNDK_demux_example("/sdcard/srv/stream/vs.mp4", 0);
+
+//	FFNDK_decode_avfile("/sdcard/srv/stream/love_mv.mpg", 0);
+//	FFNDK_decode_avfile("/sdcard/srv/stream/vs.mp4", 0);
+//	FFNDK_decode_avfile("/sdcard/srv/stream/BBC-10m_1.mkv", 0);
+	FFNDK_decode_avfile("/sdcard/srv/stream/VIDEO0001.3gp", 0);
 
 	return 0;
 }
