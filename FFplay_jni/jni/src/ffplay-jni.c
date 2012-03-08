88798b92a5c7a127a76e3ae170a3d475108b2f0a
Tommy Chen <chen.tao.tommy@gmail.com>
2012-03-08 18:20:48 +0800

    add jni. load lib ok.

--- FFplay_jni/jni/src/ffplay-jni.c
+++ FFplay_jni/jni/src/ffplay-jni.c
@@ -22,6 +22,22 @@
 #include <jni.h>
 #include <android/log.h>
 
+#include "libavutil/avstring.h"
+//#include "libavutil/colorspace.h"
+#include "libavutil/mathematics.h"
+#include "libavutil/pixdesc.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/dict.h"
+#include "libavutil/parseutils.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/avassert.h"
+#include "libavformat/avformat.h"
+//#include "libavdevice/avdevice.h"
+//#include "libswscale/swscale.h"
+//#include "libavcodec/audioconvert.h"
+#include "libavutil/opt.h"
+#include "libavcodec/avfft.h"
+#include "libswresample/swresample.h"
 /******************************************************************************/
 /*  Externs                                                                   */
 /******************************************************************************/
@@ -37,8 +53,8 @@
  jobject     l;
  jstring     t = (*env)->NewStringUTF(env, "Hello from JNI !");
  jbyteArray jarray = (*env)->NewByteArray(env, nOutSize);
- char *str = GetStringUTFChars(env, jstring, 0);
- (env)->ReleaseStringUTFChars(env, jstring, str);
+ char *str = (*env)->GetStringUTFChars(env, jstring, 0);
+ (*env)->ReleaseStringUTFChars(env, jstring, str);
 
  http://www.360doc.com/content/10/1229/17/5116642_82372464.shtml
 
@@ -47,18 +63,30 @@
 /******************************************************************************/
 /*  Local Macro Definitions                                                   */
 /******************************************************************************/
-#define DEBUG 0
+#define DEBUG 1
 
 #if DEBUG
-#  define  D(x...)  __android_log_print(ANDROID_LOG_DEBUG,"audiotest",x)
+#  define  D(x...)  __android_log_print(ANDROID_LOG_DEBUG,"ffplay-jni",x)
 #else
 #  define  D(...)  do {} while (0)
 #endif
 
-#if 1
-#  define  I(x...)  __android_log_print(ANDROID_LOG_INFO,"audiotest",x)
+#if DEBUG
+#  define  I(x...)  __android_log_print(ANDROID_LOG_INFO,"ffplay-jni",x)
 #else
 #  define  I(...)  do {} while (0)
+#endif
+
+#if DEBUG
+#  define  W(x...)  __android_log_print(ANDROID_LOG_WARN,"ffplay-jni",x)
+#else
+#  define  W(...)  do {} while (0)
+#endif
+
+#if DEBUG
+#  define  E(x...)  __android_log_print(ANDROID_LOG_ERROR,"ffplay-jni",x)
+#else
+#  define  E(...)  do {} while (0)
 #endif
 
 /******************************************************************************/
@@ -68,6 +96,17 @@
 /******************************************************************************/
 /*  Local Variables                                                           */
 /******************************************************************************/
+AVCodec *acodec, *vcodec;
+AVCodecContext *ac, *vc;
+AVStream *audio_st, *video_st;
+AVFrame *vframe, *aframe;
+AVPacket avpkt;
+AVFormatContext *fc;
+int audioidx, videoidx;
+
+int frame, got_picture, len;
+FILE *f = NULL;
+char buf[1024];
 
 /******************************************************************************/
 /*  Local Function Declarations                                               */
@@ -80,19 +119,21 @@
 /*
  * Init
  */
-jint Java_com_tommy_ffplayer_FFplayActivity_FFplayInit(JNIEnv* env,
-		jobject thiz)
+jint Java_com_tommy_ffplayer_FFplay_FFplayInit(JNIEnv* env, jobject thiz)
 {
 	D("FFplayInit called ! ");
 
 	I("\n**************************Android******************************\n");
-	I("*  FFplayer based on libffmpeg, build time: %s %s \n", __DATE__, __TIME__);
+	I(
+			"*  FFplayer based on libffmpeg, build time: %s %s \n", __DATE__, __TIME__);
 #ifdef OPT_TOMMY_NEON
 	I("***************  TOMMY OPTIMIZED USING NEON  ********************\n");
 #endif
+	I("avcodec version %d\n", avcodec_version());
+	I("avformat version %d\n", avformat_version());
 	I("*****************************************************************\n");
 
-	avcodec_init();
+//	avcodec_init();
 	avcodec_register_all();
 	av_register_all();
 
@@ -101,12 +142,747 @@ jint Java_com_tommy_ffplayer_FFplayActivity_FFplayInit(JNIEnv* env,
 }
 
 /*
- * Exit
+ * return
  */
-jint Java_com_tommy_ffplayer_FFplayActivity_FFplayExit(JNIEnv* env,
-		jobject thiz)
+jint Java_com_tommy_ffplayer_FFplay_FFplayExit(JNIEnv* env, jobject thiz)
 {
 	return 0;
+}
+
+/*
+ * OpenFile
+ */
+
+jint Java_com_tommy_ffplayer_FFplay_FFplayOpenFile(JNIEnv* env, jobject thiz,
+		jstring fname)
+{
+	int i;
+	const char *filename = (*env)->GetStringUTFChars(env, fname, 0);
+
+	av_init_packet(&avpkt);
+
+	I("avfile decoding ... %s \n", filename);
+
+	fc = avformat_alloc_context();
+	if (avformat_open_input(&fc, filename, NULL, 0) < 0)
+	{
+		I( "could not open file\n");
+		return 1;
+	}
+	(*env)->ReleaseStringUTFChars(env, fname, filename);
+
+	fc->flags |= AVFMT_FLAG_GENPTS;
+
+	if (avformat_find_stream_info(fc, 0) < 0)
+	{
+		I( "find stream failed. \n");
+		return 1;
+	}
+
+	I("nb_streams is %d\n", fc->nb_streams);
+	for (i = 0; i < fc->nb_streams; i++)
+	{
+		AVStream *st = fc->streams[i];
+		I("Stream #%d: \n", i);
+		switch (fc->streams[i]->codec->codec_type)
+		{
+		case AVMEDIA_TYPE_VIDEO:
+			videoidx = i;
+			I("Video.");
+			break;
+		case AVMEDIA_TYPE_AUDIO:
+			audioidx = i;
+			I("Audio.");
+			break;
+		default:
+			I("OTHER AVMEDIA. ");
+			break;
+		}
+		avcodec_string(buf, sizeof(buf), st->codec, 1);
+		I(
+				"\n[0x%x]:%s, \ncodec_name:'%s'. id:%05xH. tag:%08xH. time_base:%d, %d\n", st->id, buf, st->codec->codec_name, st->codec->codec_id, (st->codec->codec_tag), st->codec->time_base.num, st->codec->time_base.den);
+	};
+
+	vc = fc->streams[videoidx]->codec;
+	ac = fc->streams[audioidx]->codec;
+
+	vcodec = avcodec_find_decoder(vc->codec_id);
+	acodec = avcodec_find_decoder(ac->codec_id);
+	if (!acodec || !vcodec)
+	{
+		I( "codec not found\n");
+		return (1);
+	}
+
+	vframe = avcodec_alloc_frame();
+	aframe = avcodec_alloc_frame();
+
+	if (vcodec->capabilities & CODEC_CAP_TRUNCATED)
+		vc->flags |= CODEC_FLAG_TRUNCATED; /* we do not send complete frames */
+
+	/* For some codecs, such as msmpeg4 and mpeg4, width and height
+	 MUST be initialized there because this information is not
+	 available in the bitstream. */
+
+	/* open it */
+	if (avcodec_open2(ac, acodec, 0) < 0)
+	{
+		I( "could not open codec.");
+		return (1);
+	}
+
+	/* the codec gives us the frame size, in samples */
+
+//	f = fopen(filename, "rb");
+//	if (!f)-
+//	{
+//		I( "could not open %s\n", filename);
+//		return (1);
+//	}
+
+	I( "OpenFile done.");
+	return 0;
+}
+
+/*
+ * CloseFile
+ */
+jint Java_com_tommy_ffplayer_FFplay_FFplayCloseFile(JNIEnv* env, jobject thiz)
+{
+	fclose(f);
+	avcodec_close(ac);
+	av_free(ac);
+	avcodec_close(vc);
+	av_free(vc);
+	av_free(vframe);
+	av_free(aframe);
+
+	return 0;
+}
+
+/*
+ * DecodeFrame
+ * return the decoded pcm data.
+ */
+jbyteArray Java_com_tommy_ffplayer_FFplay_FFplayDecodeFrame(JNIEnv* env,
+		jobject thiz)
+{
+
+	while (av_read_frame(fc, &avpkt) >= 0)
+	{
+		if (avpkt.stream_index == videoidx)
+		{
+			I("skip video frame.");
+			continue;
+		}
+
+		while (avpkt.size > 0 && avpkt.stream_index == audioidx)
+		{
+			int got_audio_frame = 0;
+			AVFrame *decoded_frame = NULL;
+
+			avcodec_get_frame_defaults(aframe);
+
+			len = avcodec_decode_audio4(ac, aframe, &got_audio_frame, &avpkt);
+			if (len < 0)
+			{
+				I("avpkt.size  %d bytes left.\n", avpkt.size);
+				E("Error while decoding\n");
+//				if (ending == TRUE)
+//				{
+//					avpkt.size = 0;
+//					break;
+//				}
+				avpkt.size -= 1;
+				avpkt.data += 1;
+				continue;
+			}
+
+
+			if (got_audio_frame)
+			{
+				I(
+						"^, ch:%d, samples:%d, fmt:%d\n", ac->channels, decoded_frame->nb_samples, ac->sample_fmt);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				jbyte *outbuf = (jbyte*) aframe->data[0];
+				int outsize = av_samples_get_buffer_size(NULL, ac->channels,
+						aframe->nb_samples, ac->sample_fmt, 1);
+				jbyteArray jarray = (*env)->NewByteArray(env, outsize);
+				(*env)->SetByteArrayRegion(env, jarray, 0, outsize, outbuf);
+				return jarray;
+
+
+				/* if a frame has been decoded, output it */
+
+//				fwrite(decoded_frame->data[0], 1, data_size, outfile);
+			}
+
+			avpkt.size -= len;
+			avpkt.data += len;
+		}
+		break;
+
+//		if (frame > 1000)
+//			break;
+	}
+
+	return NULL;
 }
 
 #if 0
@@ -216,7 +992,7 @@ jbyteArray Java_com_tommy_test_audiotest_AudioTest_mp3decGetOutputPcmBuff(
 	(*env)->ReleaseByteArrayElements(env, buf, elems, 0);
 	return buf;
 #else
-	// ��jni��ֱ��newһ��byte���飬Ȼ����ú���(*env)->SetByteArrayRegion(env, bytearray, 0, len, buffer);��buffer��ֵcopy��bytearray�У�����ֱ��return bytearray�Ϳ�����
+	// 锟斤拷jni锟斤拷直锟斤拷new一锟斤拷byte锟斤拷锟介，然锟斤拷锟斤拷煤锟斤拷锟�*env)->SetByteArrayRegion(env, bytearray, 0, len, buffer);锟斤拷buffer锟斤拷值copy锟斤拷bytearray锟叫ｏ拷锟斤拷锟斤拷直锟斤拷return bytearray锟酵匡拷锟斤拷锟斤拷
 	jbyte *outbuf = (jbyte*)pcm->pOutBuffer;
 	int nOutSize = pcm->length * pcm->channels * 2;
 	jbyteArray jarray = (*env)->NewByteArray(env, nOutSize);
---
+++ FFplay_jni/src/com/tommy/ffplayer/FFplay.java
@@ -0,0 +1,192 @@
+package com.tommy.ffplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
+public class FFplay extends Activity {
+    /** Called when the activity is first created. */
+
+	/*
+	 * A native method that is implemented by the 'hello-jni' native library,
+	 * which is packaged with this application.
+	 */
+	public native int FFplayInit();
+	public native int FFplayExit();
+	public native int FFplayOpenFile(String name);
+	public native int FFplayCloseFile();
+	public native byte[] FFplayDecodeFrame();
+	
+	private final static String TAG = "ffplay-java";
+	private AudioTrack at = null;
+	private int min_buf_size;
+	private boolean bRunning = false;
+	private Thread thPlay;
+	
+	private Button mButton1, mButton2, mButton3;
+	private TextView tv1;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+   
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+		mButton1 = (Button) findViewById(R.id.button1);
+		mButton1.setOnClickListener(handler_button1);
+
+		mButton2 = (Button) findViewById(R.id.button2);
+		mButton2.setOnClickListener(handler_button2);
+		Log.i(TAG, "ffplay oncreate !");
+		
+		AudioTrack_init();
+		FFplayInit();
+        
+    }
+    
+	/*
+	 * this is used to load the 'hello-jni' library on application startup. The
+	 * library has already been unpacked into
+	 * /data/data/com.example.HelloJni/lib/libhello-jni.so at installation time
+	 * by the package manager.
+	 */
+	static
+	{
+		System.loadLibrary("ffplay_jni");
+	}
+	/*
+	 * playing wave
+	 */
+	Button.OnClickListener handler_button1 = new Button.OnClickListener()
+	{
+		@Override
+		public void onClick(View arg0)
+		{
+			try
+			{
+				FFplayOpenFile(new String("/mnt/sdcard/srv/stream/vs.mp4"));
+				
+				while(true)
+				{
+					byte[] outpcm;
+					outpcm = FFplayDecodeFrame();
+					if(outpcm == null)
+						break;
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+				}
+			} catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	};
+
+	/*
+	 * Stop
+	 */
+	Button.OnClickListener handler_button2 = new Button.OnClickListener()
+	{
+		@Override
+		public void onClick(View arg0)
+		{
+			bRunning = false;
+		}
+	};
+
+	/*
+	 * Init Audio Track.
+	 */
+	private void AudioTrack_init()
+	{
+		// Get Min Buffer Size
+		min_buf_size = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+
+		// New
+		at = new AudioTrack(AudioManager.STREAM_MUSIC, 44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, min_buf_size*2, AudioTrack.MODE_STREAM);
+		Log.d(TAG, "volume " + at.getMinVolume() + " -- " + at.getMaxVolume());
+		Log.d(TAG, "audio track's min. buffer is " + min_buf_size);
+	}
+	
+}
