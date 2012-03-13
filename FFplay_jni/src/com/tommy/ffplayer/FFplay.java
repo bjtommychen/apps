8c6e6be329ee0f6215f98467ae519b65d7d323fb
Tommy Chen <chen.tao.tommy@gmail.com>
2012-03-13 15:33:04 +0800

    add handle to udpate UI.

--- FFplay_jni/src/com/tommy/ffplayer/FFplay.java
+++ FFplay_jni/src/com/tommy/ffplayer/FFplay.java
@@ -2,8 +2,11 @@ package com.tommy.ffplayer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.util.Log;
 
@@ -32,15 +35,22 @@ public class FFplay extends Activity
 	public native String FFplayGetStreamInfo();
 
 	private final static String TAG = "ffplay-java";
+	// AUDIO TRACK
 	private AudioTrack at = null;
 	private int min_buf_size;
 	private boolean bRunning = false;
 	private Thread thPlay;
 	private int at_srate = 44100;
 	private int at_channel = AudioFormat.CHANNEL_OUT_STEREO;
-	
+	private int frame;
+	// VIEW
 	private Button mButton1, mButton2, mButton3;
-	private TextView tv1;
+	private TextView tv1, tv2, tv3, tv4;
+	private ProgressBar progbar;
+	// HANDLE
+	private Handler handler = null;
+	protected static final int GUI_UPDATE_PROGRESS = 0x100;
+	protected static final int GUI_PLAYEND = 0x101;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -51,13 +61,16 @@ public class FFplay extends Activity
 
 		mButton1 = (Button) findViewById(R.id.button1);
 		mButton1.setOnClickListener(handler_button1);
-
 		mButton2 = (Button) findViewById(R.id.button2);
 		mButton2.setOnClickListener(handler_button2);
-
-		tv1 = (TextView) findViewById(R.id.textview1);
+		tv1 = (TextView) findViewById(R.id.TextView1);
+		tv2 = (TextView) findViewById(R.id.TextView2);
+		tv3 = (TextView) findViewById(R.id.TextView3);
+		tv4 = (TextView) findViewById(R.id.TextView4);
+		progbar = (ProgressBar) findViewById(R.id.progressBar1);
 		Log.i(TAG, "ffplay oncreate !");
 
+		initHandles();
 		AudioTrack_init();
 		FFplayInit();
 
@@ -73,6 +86,37 @@ public class FFplay extends Activity
 	{
 		System.loadLibrary("ffplay_jni");
 	}
+
+	/*
+	 * Can only update UI in handle.
+	 */
+	private void initHandles()
+	{
+		// Create Main MSG handler
+		handler = new Handler()
+		{
+			@Override
+			public void handleMessage(Message msg)
+			{
+				switch (msg.what)
+				{
+					case GUI_UPDATE_PROGRESS:
+						tv3.setText("playing frame " + frame);
+						if (progbar.getProgress() >= progbar.getMax())
+							progbar.setProgress(0);
+						progbar.incrementProgressBy(1);
+						break;
+
+					case GUI_PLAYEND:
+						progbar.setProgress(0);
+						tv3.setText("play end!");
+						break;
+				}
+				super.handleMessage(msg);
+			}
+		};
+	}
+
 	/*
 	 * playing wave
 	 */
@@ -81,14 +125,17 @@ public class FFplay extends Activity
 		@Override
 		public void onClick(View arg0)
 		{
+			String filename = new String("/mnt/sdcard/srv/stream/vs.mp4"); // ("/mnt/sdcard/srv/stream/VIDEO0001.3gp"));
+
 			if (bRunning)
 				return;
 
 			at.play();
-			FFplayOpenFile(new String("/mnt/sdcard/srv/stream/vs.mp4"));
-//			FFplayOpenFile(new String("/mnt/sdcard/srv/stream/VIDEO0001.3gp"));
-			
-			tv1.setText(FFplayGetStreamInfo());
+			FFplayOpenFile(filename);
+			tv1.setText(filename);
+			// FFplayOpenFile(new String
+
+			tv4.setText(FFplayGetStreamInfo());
 			bRunning = true;
 			thPlay = new Thread()
 			{
@@ -97,6 +144,7 @@ public class FFplay extends Activity
 				{
 					try
 					{
+						frame = 0;
 						while (bRunning)
 						{
 							byte[] outpcm;
@@ -109,19 +157,23 @@ public class FFplay extends Activity
 							} else
 							{
 								if (outpcm[0] == 1)
-								{	//If audio data.
+								{ // If audio data.
 									at.write(outpcm, 40, outpcm.length - 40);
-								}
-								else
+								} else
 								{
-									Log.d(TAG, "SKIP VIDEO DATA " + outpcm.length + " BYTES." );
+									Log.d(TAG, "SKIP VIDEO DATA " + outpcm.length + " BYTES.");
 								}
 							}
+							if (frame % 5 == 0)
+								handler.sendEmptyMessage(GUI_UPDATE_PROGRESS);
+							frame++;
+
 							Thread.sleep(1);
 						}
 						at.stop();
 						FFplayCloseFile();
 						bRunning = false;
+						handler.sendEmptyMessage(GUI_PLAYEND);
 
 					} catch (Exception e)
 					{
