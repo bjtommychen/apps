be2c3c406efbc3e6fd00aa809ace1f829f228864
tommy <chen.tao.tommy@gmail.com>
2012-04-19 11:37:42 +0800

    add this

---
+++ toolbox/mkonelib/mkliball/mkonelib_fromlibs.bat
@@ -0,0 +1,29 @@
+rm -rf ./tmp 
+mkdir tmp
+
+mkdir tmp/libavformat
+cd tmp/libavformat
+ar x ../../../libavformat.a
+cd ../..
+
+mkdir tmp/libavcodec
+cd tmp/libavcodec
+ar x ../../../libavcodec.a
+cd ../..
+
+mkdir tmp/libavutil
+cd tmp/libavutil
+ar x ../../../libavutil.a
+cd ../..
+
+mkdir tmp/libswresample
+cd tmp/libswresample
+ar x ../../../libswresample.a
+cd ../..
+
+arm-linux-androideabi-ar cr libstagefright_ffmpeg.a tmp/libavformat/*.o tmp/libavcodec/*.o tmp/libavutil/*.o tmp/libswresample/*.o 
+
+#tmp/libz/*.o
+#arm-linux-androideabi-ranlib libstagefright_ffmpeg.a
+
+
