fa07a800c45490d1749d1fb77987b72f9561d274
Tommy Chen <chen.tao.tommy@gmail.com>
2012-04-26 17:57:03 +0800

    temp check in.

--- toolbox/remove_symbols_from_lib/remove_symbols_from_lib.bat
+++ toolbox/remove_symbols_from_lib/remove_symbols_from_lib.bat
@@ -10,8 +10,14 @@ cd ..
 zdobjdump -h allobjs.o
 zdnm allobjs.o
 rm allobjs_strip.o
-zdobjcopy allobjs.o allobjs_strip.o -S --keep-symbols keep_symbols.txt
-zdnm allobjs_strip.o
+rm t1.o
+rm t2.o
+zdobjcopy allobjs.o t1.o --redefine-sym _Mixer2CH_V2_800M=aaaaa --redefine-sym _Mixer2CH_800M=bbbbb  --keep-global-symbols keep_symbols.txt 
+zdnm t1.o
+zdobjcopy t1.o t2.o -xg 
+zdnm t2.o
+zdobjcopy t2.o allobjs_strip.o -S --keep-symbols keep_symbols.txt
+zdnm allobjs_strip.o 
 rm %1.new
 zdar -r %1.new allobjs_strip.o
 zdsize -t %1
