7d9580ab2eca4ecac38bb82d38f2585fcb1e970b
tommy <chen.tao.tommy@gmail.com>
2012-08-08 18:01:14 +0800

    review code. test OK.

--- devdrv/mydriver/mydriver.c
+++ devdrv/mydriver/mydriver.c
@@ -1,41 +1,23 @@
-
-#if 0
-
-/****************hellomod.c*******************************/
-#include <linux/module.h> //
-#include <linux/init.h> // init&exit相关宏
-MODULE_LICENSE("GPL");
-static int __init hello_init (void)
-{
-    printk("Hello china init/n");
-    return 0;
-}
-
-static void __exit hello_exit (void)
-{
-    printk("Hello china exit/n");
-}
-
-module_init(hello_init);
-module_exit(hello_exit);
-
-#endif
-/****************hellomod.c*******************************/
-
-
-#if 1
-
-//#define  __NO_VERSION__
-//#include<linux/version.h> /*以下是本程序包含的头文件*/
-//#include<linux/module.h>
-//#include<linux/config.h>
-//#include<asm-generic/uaccess.h>
-//#include<linux/types.h>
-//#include<linux/fs.h>
-//#include<linux/mm.h>
-//#include<linux/errno.h>
-//#include<asm-generic/segment.h>
-
+/*******************************************************************************
+*
+*    Template.c    -   Template source file
+*
+*    Copyright (c) 2012 Tommy
+*    All Rights Reserved.
+*
+*    Use of Tommy's code is governed by terms and conditions
+*    stated in the accompanying licensing statement.
+*
+*    Description:
+*
+*    Rivision Table
+* ------------------------------------------------------------------------------
+*    Name            Date            Summary
+* ------------------------------------------------------------------------------
+*    Tommy  2/27/2012  created this file.
+*
+*   $Id$
+*******************************************************************************/
 
 #include "linux/kernel.h"
 #include "linux/module.h"
@@ -46,20 +28,46 @@ module_exit(hello_exit);
 #include "linux/uaccess.h"
 #include "linux/kdev_t.h"
 
-//#include <linux/module.h>
-//#include <linux/init.h>
-//#include <linux/kernel.h>
+/******************************************************************************/
+/*  Externs                                                                   */
+/******************************************************************************/
 
+
+/******************************************************************************/
+/*  Local Macro Definitions                                                   */
+/******************************************************************************/
+MODULE_LICENSE("GPL");
+
+/******************************************************************************/
+/*  Local Type Definitions                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/*  Local Variables                                                           */
+/******************************************************************************/
 #define BUFSIZE 256  /*设备中包含的最大字符数*/
 
-char * temp;  /*该指针用于为这个虚拟的设备分配内存空间*/
-unsigned int major = 0;
+char * work_buffer;  /*该指针用于为这个虚拟的设备分配内存空间*/
+unsigned int devid_major = 0;
+
+/******************************************************************************/
+/*  Local Function Declarations                                               */
+/******************************************************************************/
+
+/******************************************************************************/
+/*  Function Definitions                                                      */
+/******************************************************************************/
 
 
-#if 1
+
+
+
+
+
 static ssize_t device_read(struct file * file,char * buf,size_t count,loff_t * f_pos)
 {
     int i;
+
     if(count>BUFSIZE)   /*如果要求读出的数目多于设备固有的数目则提示并返回*/
     {
         printk("Can't Read , the Count is to Big !\n");
@@ -67,9 +75,11 @@ static ssize_t device_read(struct file * file,char * buf,size_t count,loff_t * f
     }
     for(i = 0; i < count; i++) /*否则,进行读出操作*/
     {
-        __put_user(temp[i],buf);
+        //__put_user(work_buffer[i],buf);
+        *buf = work_buffer[i]+1;
         buf++;
     }
+    printk("device_read %d bytes.\n", count);
     return count;
 }
 
@@ -84,23 +94,26 @@ static ssize_t device_write(struct file * file,const char * buf,size_t count,lof
     }
     for(i = 0; i < count; i++) /*否则,进行写入操作*/
     {
-        __get_user(temp[i],buf);
+        __get_user(work_buffer[i],buf);
         buf++;
     }
+    printk("device_write %d bytes.\n", count);
     return count;
 }
 
 static int device_open(struct inode * inode,struct file * file) /*打开设备函数*/
 {
-    temp = (char *)kmalloc(BUFSIZE,GFP_KERNEL);  /*为设备分配内存空间*/
+    work_buffer = (char *)kmalloc(BUFSIZE,GFP_KERNEL);  /*为设备分配内存空间*/
 //    MOD_INC_USE_COUNT;
+    printk("device_open.\n");
     return 0;
 }
 
 static int device_release(struct inode * inode,struct file * file)
 {
-    kfree(temp);   /*释放设备占用内存空间*/
+    kfree(work_buffer);   /*释放设备占用内存空间*/
 //    MOD_DEC_USE_COUNT;
+    printk("device_release.\n");
     return 0;
 }
 
@@ -116,24 +129,30 @@ release:
     device_release
 };
 
-int init_module(void)  /*登记设备函数,insmod时调用*/
+static int __init  device_init_module(void)  /*登记设备函数,insmod时调用*/
 {
     int num;
+
     num = register_chrdev(0,"mydriver",&fops); /*系统自动返回一个未被占用的设备号*/
     if(num < 0)      /*登记未成功,提示并返回*/
     {
-        printk("Can't Got the Major Number !\n");
+        printk("Can't Got the devid_major Number !\n");
         return num;
     }
-    if(major == 0)
-        major = num;
+    if(devid_major == 0)
+        devid_major = num;
+
+    printk("\n\ndevice_init_module, major is %d.\n", devid_major);
+
     return 0;
 }
 
-void cleanup_module(void)  /*释放设备函数,rmmod时调用*/
+static void __exit  device_cleanup_module(void)  /*释放设备函数,rmmod时调用*/
 {
-    unregister_chrdev(major,"mydriver");
+    unregister_chrdev(devid_major,"mydriver");
+    printk("device_cleanup_module.\n");
 }
-#endif
-#endif
+
+module_init(device_init_module);
+module_exit(device_cleanup_module);
 
--- devdrv/mydriver/test.c
+++ devdrv/mydriver/test.c
@@ -1,15 +1,63 @@
+/*******************************************************************************
+*
+*    Template.c    -   Template source file
+*
+*    Copyright (c) 2012 Tommy 
+*    All Rights Reserved.
+*
+*    Use of Tommy's code is governed by terms and conditions
+*    stated in the accompanying licensing statement.
+*
+*    Description: 
+*
+*    Rivision Table
+* ------------------------------------------------------------------------------
+*    Name            Date            Summary
+* ------------------------------------------------------------------------------
+*    Tommy  2/27/2012  created this file.
+*
+*	$Id$
+*******************************************************************************/
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
+
+
+/******************************************************************************/
+/*  Externs                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/*  Local Macro Definitions                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/*  Local Type Definitions                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/*  Local Variables                                                           */
+/******************************************************************************/
+
+/******************************************************************************/
+/*  Local Function Declarations                                               */
+/******************************************************************************/
+
+/******************************************************************************/
+/*  Function Definitions                                                      */
+/******************************************************************************/
 
 int main(void)
 {
     int fd;
     int i;
+    int len;
     char *buf="we love duanduan! god bless our family!\n";
-    char buf1[22];
+    char buf1[200];
 
     fd = open("/dev/myDriver",O_RDWR); 
     if (fd == -1)
@@ -17,15 +65,31 @@ int main(void)
         printf("Can't open file \n");
         exit(-1);
     }
+    len = strlen(buf);
 
-    write(fd,buf,22);
-    read(fd,buf1,22);
+    write(fd,buf,len);
+    read(fd,buf1,len);
 
     printf("\n");
-    for(i=0; i<22; i++)
+    for(i=0; i<len; i++)
+        printf("%c",buf1[i]);
+    printf("\n");
+
+    for(i=0;i<len;i++)
+    {
+        buf1[i] = buf[i] - 1;
+    }
+
+    write(fd,buf1,len);
+    read(fd,buf1,len);
+
+    printf("\n");
+    for(i=0; i<len; i++)
         printf("%c",buf1[i]);
     printf("\n");
 
     close(fd);
+
     return 0;
+
 }
