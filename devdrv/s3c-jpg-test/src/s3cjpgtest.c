acc3cb9afeb1656c4df02497dbf31690dd29274c
tommy <chen.tao.tommy@gmail.com>
2012-08-21 16:49:30 +0800

    clean code. jpeg encode not ready now.

--- devdrv/s3c-jpg-test/src/s3cjpgtest.c
+++ devdrv/s3c-jpg-test/src/s3cjpgtest.c
@@ -38,121 +38,6 @@
 
 #include "JPGApi.h"
 
-/*
-struct s3c_platform_jpeg { 
-	unsigned int max_main_width;
-	unsigned int max_main_height;
-	unsigned int max_thumb_width;
-	unsigned int max_thumb_height;
-};
-
-static struct s3c_platform_jpeg default_jpeg_data __initdata = {
-	.max_main_width		= 2560,
-	.max_main_height	= 1920,
-	.max_thumb_width	= 0,
-	.max_thumb_height	= 0,
-};
-
-void __init s3c_jpeg_set_platdata(struct s3c_platform_jpeg *pd)
-{
-	struct s3c_platform_jpeg *npd;
-
-	if (!pd)
-		pd = &default_jpeg_data;
-
-	npd = kmemdup(pd, sizeof(struct s3c_platform_jpeg), GFP_KERNEL);
-	if (!npd)
-		printk(KERN_ERR "%s: no memory for platform data\n", __func__);
-	else
-		s3c_device_jpeg.dev.platform_data = npd;
-}
-
-static struct resource s3c_jpeg_resource[] = {
-	[0] = {
-		.start = S5PV210_PA_JPEG,
-		.end   = S5PV210_PA_JPEG + S5PV210_SZ_JPEG - 1,
-		.flags = IORESOURCE_MEM,
-	},
-	[1] = {
-		.start = IRQ_JPEG,
-		.end   = IRQ_JPEG,
-		.flags = IORESOURCE_IRQ,
-	}
-};
-
-struct platform_device s3c_device_jpeg = {
-	.name             = "s3c-jpg",
-	.id               = -1,
-	.num_resources    = ARRAY_SIZE(s3c_jpeg_resource),
-	.resource         = s3c_jpeg_resource,
-};
-
-#ifdef CONFIG_VIDEO_JPEG_V2
-static struct s3c_platform_jpeg jpeg_plat __initdata = {
-	.max_main_width		= 1280,
-	.max_main_height	= 960,
-	.max_thumb_width	= 400,
-	.max_thumb_height	= 240,
-};
-#endif
-
-#define  S5PV210_VIDEO_SAMSUNG_MEMSIZE_JPEG 		(4096 * SZ_1K)
-	[4] = {
-		.id = S5P_MDEV_JPEG,
-		.name = "jpeg",
-		.bank = 0,
-		.memsize = S5PV210_VIDEO_SAMSUNG_MEMSIZE_JPEG,
-		.paddr = 0,
-	},
-
-\\10.10.32.73\andev\cm9src\kernel\samsung\p1\arch\arm\plat-s5p\devs.c
-\\10.10.32.73\andev\cm9src\kernel\samsung\p1\arch\arm\plat-s5p\include\plat\jpeg.h
-\\10.10.32.73\andev\cm9src\kernel\samsung\p1\arch\arm\mach-s5pv210\mach-p1.c
-\\10.10.32.73\andev\cm9src\kernel\samsung\p1\arch\arm\plat-s5p\bootmem.c
-
-Tommy: 
-looks like limit is 1280x960. and this is used in s3c_jpg_plat_init() in s3c-jpeg.c
-1280x960*3 + 400*240*3 = 4Mbytes.
-can't get the jpg_reserved_mem_size in bytes, maybe about 4M bytes.
-#define jpg_reserved_mem_size		\
-	((unsigned int)s5p_get_media_memsize_bank(S5P_MDEV_JPEG, 0))
-until now, decode 1280x960 good.
-but if decode 1600x1200, looks like IOCTL_JPG_DECODE done, but system crash when try to fwrite out.
-*/
-
-/* dmesg log
-
-<6>[    2.573328] 
-<6>[    2.573981] S3C JPEG Driver, (c) 2007 Samsung Electronics
-<6>[    2.574187] JPEG driver for S5PV210
-<4>[    2.574329] s3c_jpeg_probe called. 
-<4>[    2.574431] s3c_jpg_plat_init called. 
-<7>[    2.574621] s3c_jpg_plat_init: Resolution: Main (1280 x  960), Thumb ( 400 x  240)
-<7>[    2.574731] s3c_jpg_plat_init: JPG Stream: Main(1228800 bytes @ 0x0), Thumb(98304 bytes @ 0x12c000)
-<7>[    2.574925] s3c_jpg_plat_init: YUV frame: Main(2457600 bytes @ 0x144000), Thumb(192512 bytes @ 0x39c000)
-<7>[    2.575116] s3c_jpg_plat_init: Total buffer size : 3977216 bytes
-<7>[    2.575427] s3c_jpeg_probe: JPG_Init
-
-<7>[  158.282340] s3c_jpeg_open: JPG_open 
-<7>[  158.282456] s3c_jpeg_mmap: s3c_jpeg_mmap: Reserved memory (4194304), required memory (3977216)
-<7>[  158.282463] mmap size (3391488). 
-<7>[  158.283061] s3c_jpeg_ioctl: IOCTL_JPG_GET_STRBUF
-<7>[  158.284093] s3c_jpeg_ioctl: IOCTL_JPEG_DECODE
-<7>[  158.284686] decode_jpg: enter decode_jpg function
-<7>[  158.284875] reset_jpg: s3c_jpeg_base f0800000
-<7>[  158.292851] s3c_jpeg_irq: =====enter s3c_jpeg_irq===== 
-<7>[  158.292943] s3c_jpeg_irq: int_status : 0x00000040 status : 0x00000000
-<7>[  158.293108] decode_jpg: sample_mode : 2
-<7>[  158.293194] decode_jpg: decode size:: width : 640 height : 480
-<7>[  158.293348] get_yuv_size: get_yuv_size width(640) height(480)
-<7>[  158.293464] s3c_jpeg_ioctl: IOCTL_JPG_GET_FRMBUF
-<7>[  158.315505] s3c_jpeg_release: JPG_Close
-
-
-
-
-*/
-
 
 /******************************************************************************/
 /*  Externs                                                                   */
@@ -177,6 +62,8 @@ but if decode 1600x1200, looks like IOCTL_JPG_DECODE done, but system crash when
 #define DEBUG   1
 
 #define LOOPNUM (10L)
+
+
 /******************************************************************************/
 /*  Local Type Definitions                                                    */
 /******************************************************************************/
@@ -189,7 +76,6 @@ int dev_fd;
 
 FILE    *fpin, *fpout;
 unsigned long    width, height, samplemode;
-//char    *in_buf = NULL, *out_buf = NULL;
 char    outFilename[128] ="", inFilename[128]="";
 unsigned long infileSize, outfileSize, streamSize;
 
@@ -206,32 +92,28 @@ static void show_banner(int argc, char **argv)
     printf("s3c-jpg test v %s by %s.\t", VERSION, AUTHOR);
     printf("%sbuilt on %s %s \n", " ", __DATE__, __TIME__);
 }
-
+
+
 
 static unsigned int measureTime_inMs(struct timeval *start, struct timeval *stop)
 {
-	unsigned int sec, usec, time;
+    unsigned int sec, usec, time;
 
-	sec = stop->tv_sec - start->tv_sec;
-	if(stop->tv_usec >= start->tv_usec)
-	{
-		usec = stop->tv_usec - start->tv_usec;
-	}
-   	else
-	{	
-		usec = stop->tv_usec + 1000000 - start->tv_usec;
-		sec--;
-  	}
-	time = sec*1000 + ((double)usec)/1000;
-	return time;
+    sec = stop->tv_sec - start->tv_sec;
+    if(stop->tv_usec >= start->tv_usec)
+    {
+        usec = stop->tv_usec - start->tv_usec;
+    }
+    else
+    {
+        usec = stop->tv_usec + 1000000 - start->tv_usec;
+        sec--;
+    }
+    time = sec*1000 + ((double)usec)/1000;
+    return time;
 }
 
-    
-
-/******************************************************************************/
-/*  Function Definitions                                                      */
-/******************************************************************************/
-static void initDecodeParam(void)
+static void initJpegCodecParam(void)
 {
     jCtx = (jpg_args *)malloc(sizeof(jpg_args));
     memset(jCtx, 0x00, sizeof(jpg_args));
@@ -243,38 +125,40 @@ static void initDecodeParam(void)
     jCtx->dec_param->out_format = YCBCR_420;
 
     //enc
-//    jCtx->enc_param = (jpg_enc_proc_param *)malloc(sizeof(jpg_enc_proc_param));
-//    memset(jCtx->enc_param, 0x00, sizeof(jpg_enc_proc_param));
-//    jCtx->enc_param->sample_mode = JPG_420;
-//    jCtx->enc_param->enc_type = JPG_MAIN;
-//    jCtx->enc_param->width = 320;
-//    jCtx->enc_param->height= 240;
-//    jCtx->enc_param->quality = JPG_QUALITY_LEVEL_2;
-//    jCtx->enc_param->in_format = JPG_MODESEL_YCBCR;
-//    jCtx->enc_param->sample_mode = JPG_422;
+    jCtx->enc_param = (jpg_enc_proc_param *)malloc(sizeof(jpg_enc_proc_param));
+    memset(jCtx->enc_param, 0x00, sizeof(jpg_enc_proc_param));
+    jCtx->enc_param->sample_mode = JPG_420;
+    jCtx->enc_param->enc_type = JPG_MAIN;
+    jCtx->enc_param->width = 320;
+    jCtx->enc_param->height= 240;
+    jCtx->enc_param->quality = JPG_QUALITY_LEVEL_2;
+    jCtx->enc_param->in_format = JPG_MODESEL_YCBCR;
+    jCtx->enc_param->sample_mode = JPG_422;
 
-//    //enc thum
-//    jCtx->thumb_enc_param = (jpg_enc_proc_param *)malloc(sizeof(jpg_enc_proc_param));
-//    memset(jCtx->thumb_enc_param, 0x00, sizeof(jpg_enc_proc_param));
-//    jCtx->thumb_enc_param->sample_mode = JPG_420;
-//    jCtx->thumb_enc_param->enc_type = JPG_THUMBNAIL;
-//    jCtx->thumb_enc_param->width = 320;
-//    jCtx->thumb_enc_param->height= 240;
-//    jCtx->thumb_enc_param->quality = JPG_QUALITY_LEVEL_2;
-//    jCtx->thumb_enc_param->in_format = JPG_MODESEL_YCBCR;
-//    jCtx->thumb_enc_param->sample_mode = JPG_422;
+    //enc thum
+    jCtx->thumb_enc_param = (jpg_enc_proc_param *)malloc(sizeof(jpg_enc_proc_param));
+    memset(jCtx->thumb_enc_param, 0x00, sizeof(jpg_enc_proc_param));
+    jCtx->thumb_enc_param->sample_mode = JPG_420;
+    jCtx->thumb_enc_param->enc_type = JPG_THUMBNAIL;
+    jCtx->thumb_enc_param->width = 320;
+    jCtx->thumb_enc_param->height= 240;
+    jCtx->thumb_enc_param->quality = JPG_QUALITY_LEVEL_2;
+    jCtx->thumb_enc_param->in_format = JPG_MODESEL_YCBCR;
+    jCtx->thumb_enc_param->sample_mode = JPG_422;
 
 }
 
-int main(int argc, char **argv)
+
+/******************************************************************************/
+/*  Function Definitions                                                      */
+/******************************************************************************/
+int test_jpeg_decode(char *infile, char *outfile)
 {
     int len;
-	struct timeval start;
-	struct timeval stop;
-	unsigned int	time = 0;    
+    struct timeval start;
+    struct timeval stop;
+    unsigned int    time = 0;
     int loop = LOOPNUM;
-
-    show_banner(argc, argv);
 
     strcpy(inFilename, "in.jpg");
     strcpy(outFilename, "out.yuv");
@@ -301,15 +185,7 @@ int main(int argc, char **argv)
     // 1. handle Init                                           //
     //////////////////////////////////////////////////////////////
 
-    initDecodeParam();
 
-    dev_fd = open(JPG_DRIVER_NAME, O_RDWR);
-    if(dev_fd < 0)
-    {
-        printf("JPEG Driver open failed\n");
-        return -1;
-    }
-    printf("dev_fd is %d.\n", dev_fd);
     printf("JPEG mmap %d bytes. need < reserved memory size.\n", JPG_TOTAL_BUF_SIZE);
     jCtx->mmapped_addr = (char *)MAP_FAILED;
     jCtx->mmapped_addr = (char *) mmap(0,
@@ -330,27 +206,15 @@ int main(int argc, char **argv)
     // 3. get Input buffer address                              //
     //////////////////////////////////////////////////////////////
     jCtx->dec_param->file_size = infileSize;
-//    jCtx->dec_param->data_size = infileSize;
     jCtx->in_buf_size = infileSize;
     printf("mmapped_addr : 0x%lx\n", jCtx->mmapped_addr);
     jCtx->in_buf = (char *)ioctl(dev_fd, IOCTL_JPG_GET_STRBUF, jCtx->mmapped_addr);
     printf("in_buf : 0x%lx\n", jCtx->in_buf);
 
-//    jCtx->out_buf = (char *)ioctl(dev_fd, IOCTL_JPG_GET_FRMBUF, jCtx->mmapped_addr);
-//    printf("Decodeout_buf : 0x%x  size : %d\n", jCtx->out_buf, jCtx->dec_param->data_size);
-
-    //enc
-//    jCtx->enc_param->data_size = 320*240*2;
-//    jCtx->in_buf = (char *)ioctl(dev_fd, IOCTL_JPG_GET_FRMBUF, jCtx->mmapped_addr);
-//    printf("in_buf : 0x%lx\n", jCtx->in_buf);
-
-//    jCtx->phy_in_buf = (char *)ioctl(dev_fd, IOCTL_JPG_GET_PHY_FRMBUF, jCtx->mmapped_addr);
-//    printf("phy_in_buf : 0x%lx\n", jCtx->phy_in_buf);
-
     //////////////////////////////////////////////////////////////
     // 4. put JPEG frame to Input buffer                        //
     //////////////////////////////////////////////////////////////
-    printf("read jpeg data in .");
+    printf("read jpeg data in ...");
     fread(jCtx->in_buf, 1, infileSize, fpin);
     fclose(fpin);
     printf(" done\n");
@@ -364,26 +228,21 @@ int main(int argc, char **argv)
 
         while(loop--)
         {
-    		gettimeofday(&start, NULL);
+            gettimeofday(&start, NULL);
             ioctl(dev_fd, IOCTL_JPG_DECODE, jCtx);
-    		gettimeofday(&stop, NULL);
+            gettimeofday(&stop, NULL);
             time += measureTime_inMs(&start, &stop);
             printf("No.%d, diff %d ms.\n", loop, measureTime_inMs(&start, &stop));
         }
 
         printf("Hw codec performance is %ldK pixel/second.\n", (jCtx->dec_param->width*jCtx->dec_param->height*LOOPNUM)/time);
-        
-        printf("dec_param->width : %d dec_param->height : %d\n", jCtx->dec_param->width, jCtx->dec_param->height);
 
-//        printf(" enc start.\n");
-//        jCtx->enc_param->enc_type = JPG_MAIN;
-//        ioctl(dev_fd, IOCTL_JPG_ENCODE, jCtx);
-//        printf(" enc done.\n");
+        printf("dec_param->width : %d dec_param->height : %d\n", jCtx->dec_param->width, jCtx->dec_param->height);
 
     }
     else
     {
-        // thumbnail decode, for the future work. 
+        // thumbnail decode, for the future work.
     }
 
 
@@ -418,17 +277,156 @@ int main(int argc, char **argv)
     //////////////////////////////////////////////////////////////
     munmap(jCtx->mmapped_addr, JPG_TOTAL_BUF_SIZE);
 
-    close(dev_fd);
+    return 0;
 
+}
+
+
+/*Tommy: this function not finished */
+int test_jpeg_encode(char *infile, char *outfile)
+{
+    int len;
+    struct timeval start;
+    struct timeval stop;
+    unsigned int    time = 0;
+    int loop = LOOPNUM;
+
+    strcpy(inFilename, "in.jpg");
+    strcpy(outFilename, "out.yuv");
+
+    fpin = fopen(inFilename, "rb");
+    if(fpin == NULL)
+    {
+        printf("file open error : %s\n", inFilename);
+    }
+
+    fpout = fopen(outFilename, "wb");
+    if(fpout == NULL )
+    {
+        printf("file open error : %s\n", outFilename);
+    }
+
+
+    fseek(fpin, 0, SEEK_END);
+    infileSize = ftell(fpin);
+    fseek(fpin, 0, SEEK_SET);
+    printf("infile size %d bytes . \n", infileSize);
+
+    //////////////////////////////////////////////////////////////
+    // 1. handle Init                                           //
+    //////////////////////////////////////////////////////////////
+
+
+    printf("JPEG mmap %d bytes. need < reserved memory size.\n", JPG_TOTAL_BUF_SIZE);
+    jCtx->mmapped_addr = (char *)MAP_FAILED;
+    jCtx->mmapped_addr = (char *) mmap(0,
+                                       JPG_TOTAL_BUF_SIZE,
+                                       PROT_READ | PROT_WRITE,
+                                       MAP_SHARED,
+                                       dev_fd,
+                                       0
+                                      );
+    if (jCtx->mmapped_addr == MAP_FAILED)
+    {
+        printf( "JPEG mmap failed\n");
+        return -1;
+    }
+
+    /* prepare dec param */
+    //////////////////////////////////////////////////////////////
+    // 3. get Input buffer address                              //
+    //////////////////////////////////////////////////////////////
+    printf("mmapped_addr : 0x%lx\n", jCtx->mmapped_addr);
+
+    //enc
+    jCtx->enc_param->data_size = 320*240*2;
+    jCtx->in_buf = (char *)ioctl(dev_fd, IOCTL_JPG_GET_FRMBUF, jCtx->mmapped_addr);
+    printf("in_buf : 0x%lx\n", jCtx->in_buf);
+
+
+    //////////////////////////////////////////////////////////////
+    // 4. put JPEG frame to Input buffer                        //
+    //////////////////////////////////////////////////////////////
+    printf("read jpeg data in .");
+    fread(jCtx->in_buf, 1, infileSize, fpin);
+    fclose(fpin);
+    printf(" done\n");
+
+
+    //////////////////////////////////////////////////////////////
+    // 5. Decode JPEG frame                                     //
+    //////////////////////////////////////////////////////////////
+
+    printf(" enc start.\n");
+    jCtx->enc_param->enc_type = JPG_MAIN;
+    ioctl(dev_fd, IOCTL_JPG_ENCODE, jCtx);
+    printf(" enc done.\n");
+
+
+    //////////////////////////////////////////////////////////////
+    // 6. get Output buffer address                             //
+    //////////////////////////////////////////////////////////////
+    jCtx->out_buf = (char *)ioctl(dev_fd, IOCTL_JPG_GET_FRMBUF, jCtx->mmapped_addr);
+
+    streamSize = jCtx->dec_param->data_size;
+    printf("Decodeout_buf : 0x%x  size : %d\n", jCtx->out_buf, jCtx->dec_param->data_size);
+
+    //////////////////////////////////////////////////////////////
+    // 7. get decode config.                                    //
+    //////////////////////////////////////////////////////////////
+    width = (INT32)jCtx->dec_param->width;
+    height = (INT32)jCtx->dec_param->height;
+    samplemode = (INT32)jCtx->dec_param->sample_mode;
+
+    printf("width : %d height : %d samplemode : %d\n", width, height, samplemode);
+
+    //////////////////////////////////////////////////////////////
+    // 8. wirte output file & dispaly to LCD                    //
+    //////////////////////////////////////////////////////////////
+    len = fwrite(jCtx->out_buf, 1, streamSize, fpout);
+    printf("write %d bytes.\n", len);
+    fclose(fpout);
+
+    //////////////////////////////////////////////////////////////
+    // 9. finalize handle                                      //
+    //////////////////////////////////////////////////////////////
+    munmap(jCtx->mmapped_addr, JPG_TOTAL_BUF_SIZE);
+
+    return 0;
+
+}
+
+
+int main(int argc, char **argv)
+{
+    int ret;
+
+    /***************************/
+    show_banner(argc, argv);
+
+    printf("------------------------Decoder Test Start ---------------------\n");
+    /***************************/
+    initJpegCodecParam();
+    dev_fd = open(JPG_DRIVER_NAME, O_RDWR);
+    if(dev_fd < 0)
+    {
+        printf("JPEG Driver open failed\n");
+        return -1;
+    }
+    printf("%s opened, dev_fd is %d.\n", JPG_DRIVER_NAME ,dev_fd);
+
+    /***************************/
+    ret = test_jpeg_decode(0, 0);
+
+    /***************************/
+    close(dev_fd);
     if(jCtx->dec_param != NULL)
         free(jCtx->dec_param);
 
     free(jCtx);
 
 
-
     printf("------------------------Decoder Test Done ---------------------\n");
-    printf("Done.\n");
 }
 
 
