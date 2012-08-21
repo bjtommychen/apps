e260c2b4ef0548a1b24c57cc28b58b34cf18d1a3
tommy <chen.tao.tommy@gmail.com>
2012-08-21 16:19:51 +0800

    add 1600x1200_nv12.yuv

--- YuvShow/verify.bat
+++ YuvShow/verify.bat
@@ -1,2 +1,4 @@
-./Debug/yuvshow -i 1280x960_nv12.yuv -w 1280 -h 960 -fmt yuv420_nv12
 ./Debug/yuvshow -i 800x600_iyuv.yuv -w 800 -h 600
+./Debug/yuvshow -i 1280x960_nv12.yuv -w 1280 -h 960 -fmt yuv420_nv12
+./Debug/yuvshow -i 1600x1200_nv12.yuv -w 1600 -h 1200 -fmt yuv420_nv12
+
