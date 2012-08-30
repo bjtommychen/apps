4c048380c5f6fe2910864485ed04ef76c24fcb96
tommy <chen.tao.tommy@gmail.com>
2012-08-30 18:50:36 +0800

    update CROSS compiler.

--- ffplay_android/target/armv7-android.mk
+++ ffplay_android/target/armv7-android.mk
@@ -13,7 +13,7 @@
 # Co., Ltd. The information contained herein is confidential and
 # is not to be used by or disclosed to third parties.
 #
-# Copyright (C) 2001 Verisilicon Microelectronics Co., Ltd. 
+# Copyright (C) 2001 Verisilicon Microelectronics Co., Ltd.
 # All rights reserved.
 #
 #------------------------------------------------------------------------------
@@ -25,17 +25,19 @@
 #------------------------------------------------------------------------------
 # Target tools
 #------------------------------------------------------------------------------
-CC := arm-eabi-gcc -mcpu=cortex-a8 
-AS := arm-eabi-gcc 
-AR := arm-eabi-ar -rc
-LD := arm-eabi-gcc -mcpu=cortex-a8 
-SIZE := arm-eabi-size -t
+CROSS := arm-linux-androideabi-
+
+CC := $(CROSS)gcc -mcpu=cortex-a8
+AS := $(CROSS)gcc
+AR := $(CROSS)ar -rc
+LD := $(CROSS)gcc -mcpu=cortex-a8
+SIZE := $(CROSS)size -t
 ZDB := gdb
 
 #------------------------------------------------------------------------------
 # Target defines
 #------------------------------------------------------------------------------
-TARGET_CFLAGS := -I/usr/local/include --sysroot /srv/android-ndk-r5b/platforms/android-5/arch-arm -fPIC -mandroid -DANDROID -DOS_ANDROID
-TARGET_ASFLAGS := 
-TARGET_LDFLAGS := -L/home/tommy/ffmpeg/android/armv7_neon/lib/ --sysroot /srv/android-ndk-r5b/platforms/android-5/arch-arm -fPIC -mandroid -DANDROID -DOS_ANDROID
+TARGET_CFLAGS := -I/usr/local/include --sysroot /srv/android-ndk/platforms/android-5/arch-arm -fPIC -mandroid -DANDROID -DOS_ANDROID
+TARGET_ASFLAGS :=
+TARGET_LDFLAGS := -L/home/tommy/ffmpeg/android/armv7_neon/lib/ --sysroot /srv/android-ndk/platforms/android-5/arch-arm -fPIC -mandroid -DANDROID -DOS_ANDROID
 TARGET_DEFINES :=
