e88de8ca490e20f0b7e371654d281b06f0dd502b
tommy <chen.tao.tommy@gmail.com>
2012-09-11 17:01:41 +0800

    update nfs path. test OK.

--- devdrv/mydriver/modules_prepare.sh
+++ devdrv/mydriver/modules_prepare.sh
@@ -1,5 +1,5 @@
 pushd . 
-cp /mnt/nfs/cm9src/config /mnt/nfs/cm9src/kernel/samsung/p1/.config
-cd /mnt/nfs/cm9src/kernel/samsung/p1
+cp /mnt/nfs/bejquad4/andev/cm9src/config /mnt/nfs/bejquad4/andev/cm9src/kernel/samsung/p1/.config
+cd /mnt/nfs/bejquad4/andev/cm9src/kernel/samsung/p1
 make ARCH=arm CROSS_COMPILE=arm-linux-androideabi- modules_prepare
 popd
