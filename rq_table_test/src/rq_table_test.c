42004d8ade65297ddbf64b6906403548c6ec656a
Tommy Chen <chen.tao.tommy@gmail.com>
2013-04-16 16:58:15 +0800

    rq_table_test

---
+++ rq_table_test/src/rq_table_test.c
@@ -0,0 +1,265 @@
+/*
+ ============================================================================
+ Name        : rq_table_test.c
+ Author      : tommy
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef signed int mp3d_fixed_t;
+#define RQ_BITDEPTH 	11   //11 is original one. table is 0-8191.
+struct fixedfloat {
+//	unsigned long mantissa :27;
+//	unsigned short exponent :5;
+	unsigned int mantissa;
+	unsigned int exponent;
+
+}const rq_table[] = { //size 8207 or small
+#include "rq_table_all.dat"
+		};
+
+#define TEST_START_INDEX		(1<<RQ_BITDEPTH)
+
+static void t_get_table(int idx, int* m, int* e) {
+	*m = rq_table[idx].mantissa;
+	*e = rq_table[idx].exponent;
+}
+
+static int t_get_mad(int idx, int* m, int* e) {
+	int value2, value = idx;
+	int i;
+	struct fixedfloat const *power, *power2;
+	int exp_int = 0;
+	int requantized;
+
+	value2 = value;
+	i = 0;
+	while (value2) {
+		i++;
+		value2 = value2 >> 1;
+	}
+	power = &rq_table[(int) (value >> (i - RQ_BITDEPTH))];
+	power2 = &rq_table[1 << (i - RQ_BITDEPTH)];
+	exp_int += power->exponent;
+	exp_int += power2->exponent;
+
+	requantized = (int) (((int64_t) power->mantissa * power2->mantissa) >> 28);
+	//fix the requantized
+//    requantized += 9500 * (value & ((1 << (i - RQ_BITDEPTH)) - 1));
+//    requantized += 10904 * (value & ((1 << (i - RQ_BITDEPTH)) - 1));
+
+	*m = requantized;
+	*e = exp_int;
+	return (idx & ((1 << (i - RQ_BITDEPTH)) - 1));
+}
+
+
+static int t_get_mad_mod1(int idx, int* m, int* e) {
+	int value2, value = idx;
+	int i;
+	struct fixedfloat const *power, *power2;
+	int exp_int = 0;
+	int requantized;
+	int magic = 9500;
+	struct fixedfloat const *power3;
+
+	value2 = value;
+	i = 0;
+	while (value2) {
+		i++;
+		value2 = value2 >> 1;
+	}
+	power = &rq_table[(int) (value >> (i - RQ_BITDEPTH))];
+	power2 = &rq_table[1 << (i - RQ_BITDEPTH)];
+	exp_int += power->exponent;
+	exp_int += power2->exponent;
+
+	power3 = &rq_table[(int) (value >> (i - RQ_BITDEPTH)) + 1];
+
+	if(power->exponent == power3->exponent)
+	{
+
+	}
+
+/*
+ *  2049 - 17340
+ 2896 - 19463
+ 2898 - 9734
+ 5795 - 12261
+ 5797 - 6132
+ */
+
+//	if (value > 5797)
+//		magic  = 6160;
+//	else if (value > 2898)
+//		magic = 10261;
+//	else if (value > 2049)
+//		magic = 18000;
+
+	requantized = (int) (((int64_t) power->mantissa * power2->mantissa) >> 28);
+	//fix the requantized
+    requantized += magic * (value & ((1 << (i - RQ_BITDEPTH)) - 1));
+
+	*m = requantized;
+	*e = exp_int;
+	return (idx & ((1 << (i - RQ_BITDEPTH)) - 1));
+
+/*
+Average percent diff is 0.004710%
+Max percent diff is 0.029420%
+
+ */
+}
+
+void test1() {
+	int i;
+	int diff_m, diff_exp, diff;
+	int index;
+
+	printf(" TEST_START_INDEX is %d\n", TEST_START_INDEX);
+	for (i = (TEST_START_INDEX) + 1; i < 8192; i++) {
+		diff_exp = rq_table[i].exponent - rq_table[TEST_START_INDEX].exponent;
+
+		diff = (int) (rq_table[i].mantissa << diff_exp)
+				- rq_table[TEST_START_INDEX].mantissa;
+
+		index = i - TEST_START_INDEX;
+		printf("No.%04d, %x, %x, diff 0x%08x.", index + TEST_START_INDEX,
+				diff_m, diff_exp, diff);
+
+		// result is 0x1ae20 - 0x2b322.
+		printf("\tstep diff is 0x%x.", diff / index);
+		printf("\n");
+
+	}
+}
+
+void test_mad_way() {
+	int i;
+	int index;
+	int m1, e1;
+	int m2, e2;
+	int diff, diff_abs;
+	float diff_per, diff_per_total = 0, diff_max = 0;
+
+	printf(" TEST_START_INDEX is %d\n", TEST_START_INDEX);
+	for (i = (TEST_START_INDEX) + 1; i < 8192; i++) {
+
+		index = i - TEST_START_INDEX;
+		printf("No.%04d,", index + TEST_START_INDEX);
+		t_get_table(i, &m1, &e1);
+		printf("\tTable is 0x%08x, %d.", m1, e1);
+//		t_get_mad(i, &m2, &e2);
+		t_get_mad_mod1(i, &m2, &e2);
+		if (e2 >= e1) {
+			diff = (m2 << (e2 - e1)) - m1;
+			diff_abs = abs(diff);
+			diff_per = (float) diff_abs * 100. / m1;
+			if (diff_per > diff_max)
+				diff_max = diff_per;
+			diff_per_total += diff_per;
+		} else
+			exit(1);
+		printf("\tMad is 0x%08x, %d. diff 0x%08x 0x%08x, %f%%   ", m2, e2, diff,
+				diff_abs, diff_per);
+		printf("\n");
+
+	}
+
+	printf("Summary:\n");
+	printf("Average percent diff is %f%%\n",
+			diff_per_total / (8192 - TEST_START_INDEX));
+	printf("Max percent diff is %f%%\n", diff_max);
+	/*
+	 * Average percent diff is 0.009349%
+	 Max percent diff is 0.037882%
+	 *
+	 */
+}
+
+void test_mad_get9500() {
+	int i;
+	int index, temp;
+	int m1, e1;
+	int m2, e2;
+	int diff, diff_abs;
+	float diff_per, diff_per_total = 0, diff_max = 0;
+	int count = 0;
+
+	printf(" TEST_START_INDEX is %d\n", TEST_START_INDEX);
+	for (i = (TEST_START_INDEX) + 1; i < 8192; i++) {
+
+		index = i - TEST_START_INDEX;
+		printf("No.%04d,", index + TEST_START_INDEX);
+		t_get_table(i, &m1, &e1);
+		printf("\tTable is 0x%08x, %d.", m1, e1);
+		temp = t_get_mad(i, &m2, &e2);
+		if (e2 >= e1) {
+			diff = (m1 >> (e2 - e1)) - m2;
+			diff_abs = abs(diff);
+			if (temp) {
+				if (temp)
+					diff_per = (float) diff_abs / temp;
+				else
+					diff_per = (float) diff_abs;
+				if (diff_per > diff_max)
+					diff_max = diff_per;
+				diff_per_total += diff_per;
+				count++;
+			}
+			else
+			{
+				diff_per = 0;
+			}
+		} else
+			exit(1);
+		printf("\tMad is 0x%08x, %d. %d. diff 0x%08x 0x%08x, %f%%   ", m2, e2,
+				temp, diff, diff_abs, diff_per);
+		printf("\n");
+	}
+	printf("Summary:\n");
+	printf("Average percent diff is %f%%\n",
+			diff_per_total / count);
+	printf("Max percent diff is %f%%\n", diff_max);
+
+	/*
+	 * Average percent diff is 9904.201172%
+Max percent diff is 19463.000000%
+
+ 2049 - 17340
+ 2896 - 19463
+ 2898 - 9734
+ 5795 - 12261
+ 5797 - 6132
+
+
+if RQ_BITDEPTH==10
+Average percent diff is 10127.095881%
+Max percent diff is 32734.000000%
+
+	 */
+
+}
+
+/*
+ *	main
+ */
+int main(void) {
+	int i;
+	puts("!!!Hello taaa World!!!"); /* prints !!!Hello World!!! */
+
+	for (i = (1 << 10); i < 8192; i++) {
+//		printf("index %04d, mantissa:0x%x, exponent:%d\n", i, rq_table[i].mantissa, rq_table[i].exponent);
+	}
+
+	test_mad_way();
+//	test_mad_get9500();
+
+	return EXIT_SUCCESS;
+}
