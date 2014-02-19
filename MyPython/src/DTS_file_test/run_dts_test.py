abb37623c600dcd83a3ef61ca9969462d5db591d
tommy <chen.tao.tommy@gmail.com>
2014-02-20 00:10:08 +0800

    temp check in.

--- MyPython/src/DTS_file_test/run_dts_test.py
+++ MyPython/src/DTS_file_test/run_dts_test.py
@@ -4,11 +4,12 @@
 import string
 import subprocess
 
-srcdir='E:'
-dstdir='I:\\temp\\'
-cygwin_dir='d:\\cygwin\\bin\\'
+srcdir='D:'
+dstdir='D:\\ram_mirror\\'
+cygwin_dir='c:\\cygwin\\bin\\'
 
 def external_cmd(cmd, msg_in=''):
+#     return None, None
     try:
         proc = subprocess.Popen(cmd,
                    shell=True,
@@ -26,15 +27,26 @@ def external_cmd(cmd, msg_in=''):
         return None, None
 
 def convert_cmdline(line):
-    print line
+#     print line
+    line = line.strip()
     line = line.replace('filename=', 'filename='+srcdir)
     line = line.replace('outputdir=D:\\DTS\\', 'outputdir='+dstdir )    
     
-    cmds = line.split(' ')
+    cmds = line.split()
+#     print len(cmds)
+    if len(cmds) < 3:
+        print 'parameter count < 3, error!'
+        return
+    
+#     if cmds[0].find('dts_decoder_dut') != -1:
+#         return
+    
 #     for i in range(0, len(cmds)):
 #         print i, cmds[i]
+#     print '--'
 
     if cmds[0].find('dts_decoder_dut') != -1:
+        dir2mk = ''
         for i in range(1, len(cmds)):
             if cmds[i].find('outputdir') != -1:
                 idx = cmds[i].find(dstdir)
@@ -48,21 +60,50 @@ def convert_cmdline(line):
 #         print 'Standard Error: %s' % stderr_val
         
         line =  " ".join(cmds)
-        print line        
+#         print line        
         stdout_val, stderr_val = external_cmd(line)
 #         print 'Standard Output: %s' % stdout_val
 #         print 'Standard Error: %s' % stderr_val
+    elif cmds[0].find('DTSTEnc_DUT') != -1:
+        dir2mk = dstdir + cmds[len(cmds)-1][2:]
+        cmdline = cygwin_dir +'mkdir --parents ' + dir2mk
+        stdout_val, stderr_val = external_cmd(cmdline)
+        cmds[len(cmds)-1] = cmds[len(cmds)-1][:2] + dstdir + cmds[len(cmds)-1][2:]
+        for i in range(1, len(cmds)-1):
+            if cmds[i].find('.\\Transcoder\\') != -1:
+                cmds[i] = srcdir + cmds[i]
+        line =  " ".join(cmds)
+#         print line
+        stdout_val, stderr_val = external_cmd(line)
+            
+#     print dir2mk
+    cmdline = cygwin_dir +'find ' + dir2mk + ' | grep wav | wc '
+    print cmdline            
+    stdout_val, stderr_val = external_cmd(cmdline)
+    print 'Standard Output: %s' % stdout_val
+    print stdout_val, stderr_val
+#     dir2mk = dir2mk.replace('\\', '/')
+    cmdline = cygwin_dir +'find ' + dir2mk 
+#     + ' | grep wav | xargs md5sum'
+#     cmdline = cmdline.replace('\\', '\\\\')
+    print cmdline            
+    stdout_val, stderr_val = external_cmd(cmdline)
+    print 'Standard Output: %s' % stdout_val
+    print stdout_val, stderr_val
     
 if __name__ == '__main__':
+    print 'start!'
     f = open('cmdlog.txt')
     cnt = 0
+#     print f.readlines()
     for line in f.readlines():
-        print cnt, line
-        print '---------------------------------------------------------------------------------------------------------------'
+        print 'NO.',cnt
+#         print cnt, line
+#         print '---------------------------------------------------------------------------------------------------------------'
         convert_cmdline(line)
         cnt +=1
-#         if cnt > 20:
-#             break
+        if cnt > 2:
+            break
         
     print 'total', cnt
     f.close()
