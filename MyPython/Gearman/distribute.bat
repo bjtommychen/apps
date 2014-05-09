55be585d9f38ca0fd3939bda7bddc20024dc0528
tommy <e5510@dell.com>
2014-05-09 15:59:26 +0800

    batch used by gearman

---
+++ MyPython/Gearman/distribute.bat
@@ -0,0 +1,3 @@
+copy %1 \\thor3\DTS\M8\dts_test\verifyGearman\ /Y
+copy %1 \\thor2\DTS\M8\dts_test\verifyGearman\ /Y
+copy %1 \\thor1\DTS\M8\dts_test\verifyGearman\ /Y
---
+++ MyPython/Gearman/runloop_TestWorker.bat
@@ -0,0 +1,2 @@
+rem @echo off
+for /L %%i in (1,1,1000) do python TestWorker.py 
