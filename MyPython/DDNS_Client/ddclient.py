1f3e83de8512f2bd66fdd4b13c8da00ed03ab4db
tommy <e5510@dell.com>
2014-06-05 17:27:02 +0800

    add DDNS using Oray.

---
+++ MyPython/DDNS_Client/ddclient.py
@@ -0,0 +1,14 @@
+﻿import os
+import urllib2
+import urllib
+import sys
+
+#用来激活花生壳
+if __name__ == '__main__':
+    print 'Connecting ddns server...'
+    str=urllib.urlopen("http://bj:ha@ddns.oray.com/ph/update?hostname=bj.oicp.net").read()
+    print 'Result: urlopen done, read done.'
+    print '*'*20
+    print str
+    print '*'*20
+    print 'Done.'
