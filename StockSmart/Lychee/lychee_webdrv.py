1ef6e3af145feea08ac988a36d05e81536f74582
tommy <e5510@dell.com>
2014-06-12 18:47:22 +0800

    add this for one webdriver.

---
+++ StockSmart/Lychee/lychee_webdrv.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*
+import os, sys, time
+import string
+import urllib, urllib2
+from selenium import webdriver
+import re
+import csv
+
+webd_allone = None
+
+def webdrv_init():
+    global webd_allone
+    if webd_allone == None:
+        webd_allone = webdriver.Firefox()
+    return 'webd_allonerv init\n'        
+        
+def webdrv_exit():        
+    global webd_allone
+    if webd_allone != None:
+        webd_allone.close()
+    return 'webd_allonerv exit\n'        
+        
+def webdrv_get():
+    global webd_allone
+    if webd_allone == None:
+        webd_allone = webd_alloneriver.Firefox()
+    return webd_allone
