f36c3c2a95600036dcf96b10ebc0bcf07f5ba27d
tommy <e5510@dell.com>
2014-09-28 23:52:13 +0800

    1. get last 5 days. 2. get lastday price change info.

--- StockSmart/BigData/Follows/FollowsChanges.py
+++ StockSmart/BigData/Follows/FollowsChanges.py
@@ -8,6 +8,8 @@ import stat,fnmatch
 import struct
 from pandas import DataFrame, Series
 import pandas as pd
+import urllib, urllib2
+import string
 
 reload(sys) 
 sys.setdefaultencoding('utf')
@@ -86,6 +88,32 @@ def CheckStar(name, code, chg_p1, pct_chg, chg_p2, chg_p3, LiuTongYi):
         return True
     else:
         return False
+
+def get_cn_rt_price(code):
+    code = code.replace('(','').replace(')','').replace(':','').lower()
+    url = 'http://hq.sinajs.cn/?list=%s' % code
+    # print url
+    try:
+        req = urllib2.Request(url)
+        content = urllib2.urlopen(req).read()
+    except Exception, e:
+        return ('', 0, 0, 0, 0, 0) 
+    else:
+        strs = content.decode('gbk')
+        data = strs.split('"')[1].split(',')
+        # print data
+        name = "%s" % data[0]
+        name = string.replace(name,' ','')
+        if (name):
+            return (name, float(data[1]), float(data[2]), float(data[3]), 
+                     float(data[4]), float(data[5]))
+        else:
+            return ('', 0, 0, 0, 0, 0)
+
+def get_stock_lastday_status(code):
+    name, openprice, lastclose, curr, todayhigh, todaylow = get_cn_rt_price(code)
+    diff_pct = str(round(((curr - lastclose)*100/lastclose), 2))+'%'
+    return lastclose, curr, diff_pct    
         
 def GetFollowsChanges_InRecentFiles(rawlist):
     dirfilelist = []
@@ -93,12 +121,14 @@ def GetFollowsChanges_InRecentFiles(rawlist):
         if 'stock_follows' in one:
             dirfilelist.append(one)
             
-    filelist = dirfilelist[-4:]
-    print filelist
-    dfp3 = pd.read_csv(filelist[0], names = ['name', 'code', 'follows'], skiprows=[0])
-    dfp2 = pd.read_csv(filelist[1], names = ['name', 'code', 'follows'], skiprows=[0])
-    dfp1 = pd.read_csv(filelist[2], names = ['name', 'code', 'follows'], skiprows=[0])
-    df   = pd.read_csv(filelist[3], names = ['name', 'code', 'follows'], skiprows=[0])
+    filelist = dirfilelist[-6:]
+    # print filelist
+    dfp5 = pd.read_csv(filelist[0], names = ['name', 'code', 'follows'], skiprows=[0])
+    dfp4 = pd.read_csv(filelist[1], names = ['name', 'code', 'follows'], skiprows=[0])
+    dfp3 = pd.read_csv(filelist[2], names = ['name', 'code', 'follows'], skiprows=[0])
+    dfp2 = pd.read_csv(filelist[3], names = ['name', 'code', 'follows'], skiprows=[0])
+    dfp1 = pd.read_csv(filelist[4], names = ['name', 'code', 'follows'], skiprows=[0])
+    df   = pd.read_csv(filelist[5], names = ['name', 'code', 'follows'], skiprows=[0])
     df_stockinfo   = pd.read_csv('stock_info_converted.csv', names = ['A','B','C','D','E','F','G','H','I','J','K','L','M'], skiprows=[0])
 
     list = []
@@ -112,19 +142,21 @@ def GetFollowsChanges_InRecentFiles(rawlist):
         chg_p1 = GetFollowChangesByName(df, dfp1, name, i)
         chg_p2 = GetFollowChangesByName(dfp1, dfp2, name, i)
         chg_p3 = GetFollowChangesByName(dfp2, dfp3, name, i)
+        chg_p4 = GetFollowChangesByName(dfp3, dfp4, name, i)
+        chg_p5 = GetFollowChangesByName(dfp4, dfp5, name, i)
         pct_chg = round(chg_p1*100./(follows-chg_p1), 2)
-        line = name, code, chg_p1, pct_chg, chg_p2, chg_p3        
+        line = name, code, chg_p1, pct_chg, chg_p2, chg_p3, chg_p4, chg_p5
         list.append(line)
-    print "*** Result ***"
-    print len(list), len(list[0])
+    print "*** Result ***",
+    print len(list), len(list[0]),
     list.sort(key=lambda data : data[2], reverse=True)
     print 'sorted.'
     rlist = list[:100]
     for one in rlist:
-        name, code, chg_p1, pct_chg, chg_p2, chg_p3 = one
+        name, code, chg_p1, pct_chg, chg_p2, chg_p3, chg_p4, chg_p5 = one
         LiuTongYi= GetLiuTong_fromInfos(df_stockinfo, one[1])/10000
         if CheckStar(name, code, chg_p1, pct_chg, chg_p2, chg_p3, LiuTongYi):
-            print  '%-10s'%one[0].decode('gbk'), one[1:], LiuTongYi
+            print  '%-10s'%one[0].decode('gbk'), one[1], one[2], str(one[3])+'%', one[4:], str(LiuTongYi)+u'亿', get_stock_lastday_status(one[1])
     print filelist
     
 if  __name__ == '__main__':
@@ -133,3 +165,4 @@ if  __name__ == '__main__':
     print '#'*60      
     # GetFollowsChanges_InFileList(getFileList('./', 'stock_follows*.csv', False))
     GetFollowsChanges_InRecentFiles(getFileList('./', '*.csv', False))
+    # print get_stock_lastday_status('sh600036')
