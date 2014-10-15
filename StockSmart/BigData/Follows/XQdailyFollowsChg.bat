5c518a81d46b93d7154eaaa69e23a3b81b744ec9
tommy <e5510@dell.com>
2014-10-15 22:42:18 +0800

    add hk, and us.

--- StockSmart/BigData/Follows/XQdailyFollowsChg.bat
+++ StockSmart/BigData/Follows/XQdailyFollowsChg.bat
@@ -1,5 +1,14 @@
 .\GetStockFollows.py 
 FollowsChanges.py  > body.txt
+
+.\GetStockFollows_us.py 
+FollowsChanges_us.py  >> body.txt
+
+.\GetStockFollows_hk.py 
+FollowsChanges_hk.py  >> body.txt
+
 cat body.txt
 xq_follows_sendmail.py "Ñ©Çò¹Ø×¢¸ú×Ù@xueqiu#" body.txt
+
+cp *.csv f:\KuaiDisk\StockSmart\follows\ -n
 pause
