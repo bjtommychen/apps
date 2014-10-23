ed78321d0af18f7dcd9bbb7362b55ca61a1df858
tommy <e5510@dell.com>
2014-10-23 19:21:53 +0800

    echo '' >> body.txt

--- StockSmart/BigData/Follows/XQdailyFollowsChg.bat
+++ StockSmart/BigData/Follows/XQdailyFollowsChg.bat
@@ -1,19 +1,22 @@
 .\GetStockFollows.py 
 echo '[ China Market ]' > body.txt
 FollowsChanges.py  >> body.txt
+echo '' >> body.txt
 
 .\GetStockFollows_us.py 
 echo '[ US Market ]' >> body.txt
 FollowsChanges_us.py  >> body.txt
+echo '' >> body.txt
 
 .\GetStockFollows_hk.py 
 echo '[ HK Market ]' >> body.txt
 FollowsChanges_hk.py  >> body.txt
+echo '' >> body.txt
 
 cat body.txt
 xq_follows_sendmail.py "Ñ©Çò¹Ø×¢¸ú×Ù@xueqiu#" body.txt
 
 cp *.csv f:\KuaiDisk\StockSmart\follows\ -n
-echo 'wait 600s...'
+echo 'wait 60s...'
 sleep 60
 rem pause
