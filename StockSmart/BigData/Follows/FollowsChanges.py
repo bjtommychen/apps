ee0d11c1e2bc322959f3db4f599cdbc3066a21a5
tommy <e5510@dell.com>
2014-11-19 22:52:03 +0800

    Get infostring from xq website.

--- StockSmart/BigData/Follows/FollowsChanges.py
+++ StockSmart/BigData/Follows/FollowsChanges.py
@@ -10,6 +10,9 @@ from pandas import DataFrame, Series
 import pandas as pd
 import urllib, urllib2
 import string
+import re
+import csv
+import requests
 
 reload(sys) 
 sys.setdefaultencoding('utf')
@@ -84,9 +87,56 @@ def GetLiuTong_fromInfos(df1, name):
                 return 0.0
     return 0.
     
+def convert_num(string):
+    return str(string)
+    
+def get_StockInfo(code):
+    url = 'http://xueqiu.com/S/%s' % code
+    # print url
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36'}
+        r = requests.get(url,timeout=5,headers=headers)
+        data = r.content
+        # print r.encoding
+        r.close()
+    except Exception, e:
+        return []
+
+    pos1 = data.find('span class="stockName"')        
+    pos2 = data.find('</table>', pos1)  
+    # print pos1, pos2
+    if pos1 == -1 and pos2 == -1:
+        return []
+    # print data[pos1:pos2].encode('gbk')
+    match = re.compile(r'(?<=<span>).*?(?=<\/span>)')
+    r = re.findall(match, data[pos1:pos2].encode('gbk'))
+    # print len(r), r
+    # for line in r:
+        # print line.decode('gbk')
+    if len(r) < 19:
+        return []
+    # 52周最高, 52周最低, 总市值, 每股净资产, 市盈率, 总股本, 每股收益, 市净率, 30日均量, 流通股本, 股息率, 市销率
+    infoline = ''
+    # infoline.append(code)
+    # infoline.append(convert_num(r[3]))
+    # infoline.append(convert_num(r[5]))
+    # infoline.append(convert_num(r[8]))
+    # infoline.append(convert_num(r[9]))
+    # infoline.append(convert_num(r[10]))
+    # infoline.append(convert_num(r[12]))
+    # infoline.append(convert_num(r[13]))
+    # infoline.append(convert_num(r[14]))
+    # infoline.append(convert_num(r[15]))
+    # infoline.append(convert_num(r[16]))
+    # infoline.append(convert_num(r[17]))
+    # infoline.append(convert_num(r[18]))
+    # print infoline
+    infoline += r[16].decode('gbk')
+    return infoline    
+    
 def CheckStar(name, code, chg_p1, pct_chg, chg_p2, chg_p3, LiuTongYi):
     # print type(pct_chg), pct_chg, chg_p1, LiuTongYi
-    if chg_p1 > 100 and pct_chg > 2.0 and LiuTongYi < 180:
+    if chg_p1 > 100 and pct_chg > 2.0: # and LiuTongYi < 180:
         return True
     else:
         return False
@@ -203,9 +253,16 @@ def GetFollowsChanges_InRecentFiles(rawlist):
     rlist = list[:100]
     for one in rlist:
         name, code, chg_p1, pct_chg, chg_p2, chg_p3, chg_p4, chg_p5, chg_p6, chg_p7 = one
-        LiuTongYi= GetLiuTong_fromInfos(df_stockinfo, one[1])/10000
+        xq_code = code.replace('(','').replace(')','').replace(':','').lower()
+        # print name, code, xq_code
+        stock_info_str = get_StockInfo(xq_code)
+        LiuTongYi = 0
+        # LiuTongYi= GetLiuTong_fromInfos(df_stockinfo, one[1])/10000
+        # if CheckStar(name, code, chg_p1, pct_chg, chg_p2, chg_p3, LiuTongYi):
+            # print  '%-10s'%one[0].decode('gbk'), one[1], ',', one[2], ',[', float('%.1f' % (chg_p1/GetFollowsMeanByCode(dirfilelist, code))),'x ]', str(one[3])+'%', ',', one[4:], u'流通股本'+str(LiuTongYi)+u'亿', get_stock_lastday_status(one[1])
         if CheckStar(name, code, chg_p1, pct_chg, chg_p2, chg_p3, LiuTongYi):
-            print  '%-10s'%one[0].decode('gbk'), one[1], ',', one[2], ',[', float('%.1f' % (chg_p1/GetFollowsMeanByCode(dirfilelist, code))),'x ]', str(one[3])+'%', ',', one[4:], str(LiuTongYi)+u'亿', get_stock_lastday_status(one[1])
+            print  '%-10s'%one[0].decode('gbk'), one[1], ',', one[2], ',[', float('%.1f' % (chg_p1/GetFollowsMeanByCode(dirfilelist, code))),'x ]', str(one[3])+'%', ',', one[4:], u'流通股本'+stock_info_str, get_stock_lastday_status(one[1])
+
     print filelist
     
 if  __name__ == '__main__':
