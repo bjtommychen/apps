cfcc3008cf8e902eb7f80aad1414839e837383d5
tommy <e5510@dell.com>
2014-11-22 23:55:16 +0800

    get data first.

--- StockSmart/BigData/Follows/XQdailyFollowsChg_6AM.bat
+++ StockSmart/BigData/Follows/XQdailyFollowsChg_6AM.bat
@@ -1,15 +1,17 @@
 chcp 936
 .\GetStockFollows.py 
+.\GetStockFollows_hk.py 
+rem .\GetStockFollows_us.py 
+cp data/*.csv f:\KuaiDisk\StockSmart\follows\data\ -n
+
 echo '[ China Market --- PreOpen ]' > body.txt
 FollowsChanges.py  >> body.txt
 echo '' >> body.txt
 
-rem .\GetStockFollows_us.py 
 echo '[ US Market --- PostClose ]' >> body.txt
 FollowsChanges_us.py  >> body.txt
 echo '' >> body.txt
 
-.\GetStockFollows_hk.py 
 echo '[ HK Market --- PreOpen ]' >> body.txt
 FollowsChanges_hk.py  >> body.txt
 echo '' >> body.txt
@@ -18,7 +20,6 @@ chcp 936
 cat body.txt
 xq_follows_sendmail.py "Ñ©Çò¹Ø×¢¸ú×ÙÔçÉÏºÃ@xueqiu#" body.txt
 
-cp data/*.csv f:\KuaiDisk\StockSmart\follows\data\ -n
 echo 'wait 60s...'
 sleep 60
 rem pause
--- StockSmart/BigData/Follows/XQdailyFollowsChg_8PM.bat
+++ StockSmart/BigData/Follows/XQdailyFollowsChg_8PM.bat
@@ -1,15 +1,17 @@
 chcp 936
 rem .\GetStockFollows.py 
+.\GetStockFollows_us.py 
+rem .\GetStockFollows_hk.py 
+cp data/*.csv f:\KuaiDisk\StockSmart\follows\data\ -n
+
 echo '[ China Market --- PostClose ]' > body.txt
 FollowsChanges.py  >> body.txt
 echo '' >> body.txt
 
-.\GetStockFollows_us.py 
 echo '[ US Market --- PreOpen ]' >> body.txt
 FollowsChanges_us.py  >> body.txt
 echo '' >> body.txt
 
-rem .\GetStockFollows_hk.py 
 echo '[ HK Market --- PostClose ]' >> body.txt
 FollowsChanges_hk.py  >> body.txt
 echo '' >> body.txt
@@ -18,7 +20,6 @@ chcp 936
 cat body.txt
 xq_follows_sendmail.py "Ñ©Çò¹Ø×¢¸ú×ÙÍíÉÏºÃ@xueqiu#" body.txt
 
-cp data/*.csv f:\KuaiDisk\StockSmart\follows\data\ -n
 echo 'wait 60s...'
 sleep 60
 rem pause
