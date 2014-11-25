3b23d7e153b0e542139765273cab312e83e09ded
Tommy Chen <chen.tao.tommy@gmail.com>
2014-11-25 15:49:04 +0800

    add us stock list.

--- StockSmart/BigData/Follows/GetStockList.py
+++ StockSmart/BigData/Follows/GetStockList.py
@@ -105,6 +105,56 @@ def get_StockList_hk():
         csvWriter.writerow(line)
     fcsv.close()  
 
+def get_StockList_us():
+    ### Get Data ###
+    url = 'http://vip.stock.finance.sina.com.cn/usstock/ustotal.php'
+    if True:   # Set True to get webpage, False to get data from File.
+        try:
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36'}
+            r = requests.get(url,timeout=5,headers=headers)
+            data = r.content
+            print r.encoding
+            r.close()
+            if True: # set True to save data for later use.
+                fp = open("getstocklist.html", 'wb');
+                fp.write(data)
+                fp.close()
+        except Exception, e:
+            return 'Error'
+    else:
+        fp = open("getstocklist.html", 'rb');
+        data = fp.read()
+        fp.close()
+        print 'get data from getstocklist.html ', len(data), 'bytes.'
+    ### Process List ###
+    match = re.compile(r'.*?(?=<\/a>)')
+    r = re.findall(match, data)
+    print len(r)
+    list=[]
+    for one in r:
+        pos1 = one.find('>')
+        pos2 = one.rfind('<')
+        # print one
+        one = one[pos1+1:pos2]
+        # print one
+        name = one[:one.find('(')]
+        code = one[one.find('('):].replace('(','').replace(')','')
+        code = code.strip()
+        if len(code) == 0:
+            continue
+        line = code, name
+        # print '[',code, ']', name
+        if len(code) > 0 and code[0].isalpha() and name.find('ETF')==-1:
+            # print '[',code, ']', name
+            list.append(line)
+    # print len(list),list                
+    print 'get stock total', len(list)
+    fcsv = open('stocklist_us.csv', 'wb')
+    csvWriter = csv.writer(fcsv)
+    for line in list:
+        csvWriter.writerow(line)
+    fcsv.close()  
+    
 ######################################################
 if  __name__ == '__main__':
     print 'Start !'
@@ -112,5 +162,7 @@ if  __name__ == '__main__':
     get_StockList_cn()
     print '*** get_StockList_cn ***'
     get_StockList_hk()
+    print '*** get_StockList_us ***'
+    get_StockList_us()
     print 'Completed !'        
         
