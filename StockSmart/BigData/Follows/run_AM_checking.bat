d42c8d8e6007957e0706f2ae53a2392483d29f47
Tommy Chen <chen.tao.tommy@gmail.com>
2014-11-28 11:43:26 +0800

    update.

--- StockSmart/BigData/Follows/run_AM_checking.bat
+++ StockSmart/BigData/Follows/run_AM_checking.bat
@@ -1,4 +1,7 @@
+title run_AM_checking
+time /t
 d:\workspace\apps\StockSmart\BigData\Follows\FollowsChanges.py
 d:\workspace\apps\StockSmart\BigData\Follows\FollowsChanges_hk.py
 time /t
+python d:\beep.py
 pause
--- StockSmart/BigData/Follows/run_PM_checking.bat
+++ StockSmart/BigData/Follows/run_PM_checking.bat
@@ -1,2 +1,6 @@
+title run_PM_checking
+time /t
 d:\workspace\apps\StockSmart\BigData\Follows\FollowsChanges_us.py
+time /t
+python d:\beep.py
 pause
