415d5638ef2329d033bd97ff3b3dd4a9d32677c9
Tommy Chen <chen.tao.tommy@gmail.com>
2014-12-04 10:59:26 +0800

    add path.

--- StockSmart/BigData/Follows/copy2ec2.bat
+++ StockSmart/BigData/Follows/copy2ec2.bat
@@ -1,1 +1,1 @@
-pscp -i myec2.ppk %1 ubuntu@bjtommychen.oicp.net:/home/ubuntu/script/longan 
+D:\workspace\apps\StockSmart\BigData\Follows\pscp -i D:\workspace\apps\StockSmart\BigData\Follows\myec2.ppk %1 ubuntu@bjtommychen.oicp.net:/home/ubuntu/script/longan 
