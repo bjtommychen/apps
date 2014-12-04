d438d84807c96032e50b853d487a252a58d74d4e
tommy <e5510@dell.com>
2014-12-04 10:47:12 +0800

    add debug

--- StockSmart/BigData/Follows/XQdailyFollowsChg_6AM.bat
+++ StockSmart/BigData/Follows/XQdailyFollowsChg_6AM.bat
@@ -16,14 +16,20 @@ echo '[ HK Market --- PreOpen ]' >> body.txt
 FollowsChanges_hk.py  >> body.txt
 echo '' >> body.txt
 
-cp watch_*.csv d:\workspace\apps\StockSmart\Longan\
-cp watch_*.csv f:\KuaiDisk\StockSmart\follows\
-call copy2ec2.bat watch_*.csv
+cp watch_*.csv d:\workspace\apps\StockSmart\Longan\ >> DailyLog.txt
+cp watch_*.csv f:\KuaiDisk\StockSmart\follows\ >> DailyLog.txt
+call copy2ec2.bat watch_*.csv  >> DailyLog.txt
 
 chcp 936
 cat body.txt
 cp body.txt f:\KuaiDisk\StockSmart\follows\
 xq_follows_sendmail.py "Ñ©Çò¹Ø×¢¸ú×ÙÔçÉÏºÃ@xueqiu#" body.txt
+
+call copy2ec2.bat watch_*.csv  >> DailyLog.txt
+sleep 60
+call copy2ec2.bat watch_*.csv  >> DailyLog.txt
+sleep 60
+call copy2ec2.bat watch_*.csv  >> DailyLog.txt
 
 echo 'wait 60s...'
 sleep 60
