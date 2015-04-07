abb2a48917ac2005863fca1884404911da422d53
Tommy Chen <chen.tao.tommy@gmail.com>
2015-04-07 22:23:11 +0800

    update.

--- StockSmart/BigData/Follows/run_daily.bat
+++ StockSmart/BigData/Follows/run_daily.bat
@@ -5,10 +5,10 @@ python ..\UpdateCSV_withQMdata.py -opath output_qda -ipath input_qda
 beep
 
 REM delay 10m for Kuaidisk sync done.
-sleep 5m
+REM sleep 5m
 cp e:/KuaiDisk/StockSmart/follows/data/*.csv ./data -n
 cp e:/KuaiDisk/StockSmart/follows/hold*.csv . -f
 cp e:/KuaiDisk/StockSmart/follows/watch*.csv . -f
 rm -f save_png/*
 python SaveWatchList2PNG.py
-REM pause
+pause
