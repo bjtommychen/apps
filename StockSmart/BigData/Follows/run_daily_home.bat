0f48cae867e0bcced7e385b3a4a36f4f2794dbb7
Tommy Chen <chen.tao.tommy@gmail.com>
2015-04-20 19:12:52 +0800

    add

---
+++ StockSmart/BigData/Follows/run_daily_home.bat
@@ -0,0 +1,19 @@
+REM delay 10m for Kuaidisk sync done.
+sleep 1m
+
+REM Get QMdata.
+python GetQDAdata.py
+python ..\UpdateCSV_withQMdata.py -opath output_qda -ipath input_qda
+python GetQDAdata1min.py
+python ..\UpdateCSV_withQMdata1min.py -opath output_qda1m -ipath input_qda1m
+python GetTodaySpurtList.py
+beep
+
+REM delay 10m for Kuaidisk sync done.
+REM sleep 5m
+cp e:/KuaiDisk/StockSmart/follows/data/*.csv ./data -n
+cp e:/KuaiDisk/StockSmart/follows/hold*.csv . -f
+cp e:/KuaiDisk/StockSmart/follows/watch*.csv . -f
+REM rm -f save_png/*
+python SaveWatchList2PNG.py
+REM pause
