e9c9416feffd1a7c9ad03e132c37d2802450f522
Tommy Chen <chen.tao.tommy@gmail.com>
2015-04-20 22:10:56 +0800

    python FindSidewaysLatent.py

--- StockSmart/BigData/Follows/run_daily_home.bat
+++ StockSmart/BigData/Follows/run_daily_home.bat
@@ -7,13 +7,14 @@ python ..\UpdateCSV_withQMdata.py -opath output_qda -ipath input_qda
 python GetQDAdata1min.py
 python ..\UpdateCSV_withQMdata1min.py -opath output_qda1m -ipath input_qda1m
 python GetTodaySpurtList.py
-beep
+python FindSidewaysLatent.py
+REM beep
 
 REM delay 10m for Kuaidisk sync done.
 REM sleep 5m
-cp e:/KuaiDisk/StockSmart/follows/data/*.csv ./data -n
-cp e:/KuaiDisk/StockSmart/follows/hold*.csv . -f
-cp e:/KuaiDisk/StockSmart/follows/watch*.csv . -f
+cp f:/KuaiDisk/StockSmart/follows/data/*.csv ./data -n
+cp f:/KuaiDisk/StockSmart/follows/hold*.csv . -f
+cp f:/KuaiDisk/StockSmart/follows/watch*.csv . -f
 REM rm -f save_png/*
 python SaveWatchList2PNG.py
 REM pause
