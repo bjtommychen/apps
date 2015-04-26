c0ae1613176345953651bd04939537aa8c08064c
Tommy Chen <chen.tao.tommy@gmail.com>
2015-04-26 23:33:51 +0800

    add title.

--- StockSmart/BigData/Follows/run_daily_home_8pm.bat
+++ StockSmart/BigData/Follows/run_daily_home_8pm.bat
@@ -1,12 +1,15 @@
 REM RUN IT DAILY MORNING IN OFFICE
-title RUN IT DAILY at Home
+title RUN IT DAILY at Home 8pm
 REM delay 10m for Kuaidisk sync done.
 sleep 5s
 
 REM Get QMdata.
+title running GetQDAdata1min.py 
 python GetQDAdata1min.py
 REM python ..\UpdateCSV_withQMdata1min.py -opath output_qda1m -ipath input_qda1m
+title running GetTodaySpurtList.py 
 python GetTodaySpurtList.py
+title running FindSidewaysLatent.py 
 python FindSidewaysLatent.py
 REM beep
 
@@ -18,4 +21,5 @@ d:\cygwin\bin\mkdir -p save_png/spurt
 d:\cygwin\bin\mkdir -p save_png/sideway
 d:\cygwin\bin\mkdir -p save_png/catchspurt
 REM rm -f save_png/*
+title running SaveWatchList2PNG.py 
 python SaveWatchList2PNG.py
