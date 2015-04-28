f37464ee1ec62596cbb11b22d90e8db894f08888
Tommy Chen <chen.tao.tommy@gmail.com>
2015-04-29 00:16:24 +0800

    updated

--- StockSmart/BigData/Follows/run_daily_home_8pm.bat
+++ StockSmart/BigData/Follows/run_daily_home_8pm.bat
@@ -16,10 +16,12 @@ REM beep
 REM delay 10m for Kuaidisk sync done.
 REM sleep 5m
 cp f:/KuaiDisk/StockSmart/follows/hold*.csv . -f
-mv save_png/* save_png_backup
+d:\cygwin\bin\mv save_png save_png_backup
 d:\cygwin\bin\mkdir -p save_png/spurt
 d:\cygwin\bin\mkdir -p save_png/sideway
 d:\cygwin\bin\mkdir -p save_png/catchspurt
 REM rm -f save_png/*
 title running SaveWatchList2PNG.py 
 python SaveWatchList2PNG.py
+title Done
+echo Done
